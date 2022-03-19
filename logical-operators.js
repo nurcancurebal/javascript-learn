727f628d4f7f4632d747475294a45da532fb6ded
Nurcan Cürebal <nurcancurebal@hotmail.com>
2022-03-20 00:02:24 +0300

    .

--- logical-operators.js
+++ logical-operators.js
@@ -1,4 +1,5 @@
 /* 
+mantıksal operatörler
 && = ampersand (ve)
 || = pipe (ya da)
 !  = negation (olumsuzlama) 
@@ -11,3 +12,14 @@ let checkk = 4>3 && 10<5
 console.log (checkk) // false
 let checkkk = 4<3 && 10<5
 console.log (checkkk) // false,  true dönmesi için her iki tarafında her zaman  
+
+
+ let a = 4>3 || 10>5
+console.log (a) // true
+let aa = 4>3 || 10<5
+console.log (aa) // true
+let aaa = 4<3 || 10<5
+console.log (aaa) //false , false dönmesi için ikisininde yanlış olması gerekir
+
+let ismarried =!false
+console.log(ismarried) //  aslında evli olmamasına rağmen true döndü
