43377a61dcaaa246b24fe77a6266d8fc1d15dfbc
Nurcan Cürebal <nurcancurebal@hotmail.com>
2022-03-20 22:56:40 +0300

    .

---
+++ date.object.js
@@ -0,0 +1,55 @@
+//  Method        Description Examples
+//getFullYear()     (yyyy)      2022
+// getMounth()      (0-11)      0
+// getDate()        (1-31)      4
+//getHours()        (0-23)      0
+//getMinutes()      (0-59)      56
+//getSeconds()      (0-59)      41
+//getMilisecond()   (0-999)     341
+//getTime()         (miliseconds since january 1, 1970)   1578092201341
+//getDay            (0-6)       6 (pazar gününü 0 alır)
+
+
+// const datee = new Date()
+// console.log(datee.getFullYear()) // 2022
+// console.log(datee.getMonth())   // 2
+// console.log(datee.getMinutes())  //59
+
+const date = new Date()
+
+let year = date.getFullYear(),
+    mounth = date.getMonth(),
+    day = date.getDate(),
+    hour = date.getHours(),
+    minute = date.getMinutes(),
+    Second = date.getSeconds(),
+    dayNumber = date.getDay();
+
+let months = [
+    'ocak',
+    'şubat',
+    'mart',
+    'nisan',
+    'mayıs',
+    'haziran',
+    'temmuz',
+    'ağustos',
+    'eylül',
+    'ekim',
+    'kasım',
+    'aralık'
+];
+
+let days = [
+    'pazar',
+    'pazartesi',
+    'salı',
+    'çarşamba',
+    'perşembe',
+    'cuma',
+    'cumartesi'
+];
+
+let humanreadabledate = `${day} ${months[mounth]} ${year}, ${days[dayNumber]} ${hour}:${minute}:${Second}`
+
+console.log(humanreadabledate)
---
+++ typeof-exercises.js
@@ -0,0 +1,14 @@
+let firstName = 'nurcan'
+let lastName = 'cürebal'
+let country = 'türkiye'
+let city = 'istanbul'
+let age = 27
+let isMarried = true
+let year = 2022
+console.log(typeof firstName)
+console.log(typeof lastName)
+console.log(typeof country)
+console.log(typeof city)
+console.log(typeof age)
+console.log(typeof isMarried)
+console.log(typeof year)
