0c732553696334d3f3754007ad18a031891e874c
Nurcan Cürebal <nurcancurebal@hotmail.com>
2022-03-21 15:13:10 +0300

    .

--- date-exp.js
+++ date-exp.js
@@ -1,10 +1,35 @@
 const date = new Date()
-let year= date.getFullYear, //! değişken atarken hepsinin başına let yazmamamızın sebebi?
- mounth= date.getMonth,
- dayNumber= date.getDate,
- hour= date.getHours,
- minute= date.getMinutes,
- second= date.getSeconds,
- day= date.getDay
-let dateTime= `${year}` //! year değişkenini dateTime değişkenini atama işlemi bu şekilde mi yapılır?
+let year= date.getFullYear(), //! değişken atarken hepsinin başına let yazmamamızın sebebi?
+ mounth= date.getMonth(),
+ dayNumber= date.getDate(),
+ hour= date.getHours(),
+ minute= date.getMinutes(),
+ second= date.getSeconds(),
+ day= date.getDay()
+
+ let months = [
+    'ocak',
+    'şubat',
+    'mart',
+    'nisan',
+    'mayıs',
+    'haziran',
+    'temmuz',
+    'ağustos',
+    'eylül',
+    'ekim',
+    'kasım',
+    'aralık']
+
+
+    let days = [
+        'pazar',
+        'pazartesi',
+        'salı',
+        'çarşamba',
+        'perşembe',
+        'cuma',
+        'cumartesi']
+
+let dateTime= ` ${dayNumber} ${months[mounth]} ${year} ${hour}:${minute}:${second} `
 console.log(dateTime)
---
+++ ucgenin-cevresi-exp.js
@@ -0,0 +1,4 @@
+let a = 5
+let b = 4
+let c = 3
+console.log('üçgenin çevresi:', a+b+c)
