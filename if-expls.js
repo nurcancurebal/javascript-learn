caa38e42adc7535816d186a04d9f9294d04007a7
Nurcan Cürebal <nurcancurebal@hotmail.com>
2022-03-23 00:13:54 +0300

    .

--- if-expls.js
+++ if-expls.js
@@ -1,1 +1,24 @@
-let day= monday
+let girdi = "Cumartesi";
+
+girdi = girdi.toLowerCase();
+
+const days = [
+    "pazartesi",
+    "salı",
+    "çarşamba",
+    "perşembe",
+    "cuma",
+    "cumartesi",
+    "pazar"
+];
+
+const dizininKonumu = days.findIndex(item => item == girdi);
+
+if (dizininKonumu <= 4) {
+    console.log("İş Günü.")
+} else if (dizininKonumu >= 4) {
+    console.log("Tatil Günü.")
+} else {
+    console.log("Gün bulunamadı.")
+}
+
---
+++ tipler.js
@@ -0,0 +1,33 @@
+const yaziTipiString = "Hello, World!"; // ! basit seviye geliştiginde kullanacaksın.
+
+const sayiTipiNumber = 55; // ! basit seviye geliştiginde kullanacaksın.
+
+const dogrulamaTipiBoolean = true; // ! basit seviye geliştiginde kullanacaksın.
+
+const dizilerTipiArray = [1,2,3,4]; // ! basit seviye geliştiginde kullanacaksın.
+
+const nesneTipiObject = { isim: "Nurcan", soyisim: "Curebal"}; // ! basit seviye geliştiginde kullanacaksın.
+
+const bozukMatematikselIslemNaN = NaN; // ! basit seviye geliştiginde kullanacaksın.
+
+const yazilimsalBosGelenIfadeTipiNull = null; // ! orta seviye geliştiginde kullanacaksın.
+
+const atanmamisVeriTipiUndefined = undefined; // ! basit seviye geliştiginde kullanacaksın.
+
+const cagirilabilirKodBloguTipiFunctionOne = function(){  // ! orta seviye geliştiginde kullanacaksın.
+    return "Merhaba";
+};
+
+function cagirilabilirKodBloguTipiFunctionTwo (){  // ! basit seviye geliştiginde kullanacaksın.
+    console.log("Merhaba");
+};
+
+const cagirilabilirKodBloguTipiFunctionThree = () => { // ! orta seviye geliştiginde kullanacaksın.
+    return "Array function"
+}
+
+const gelismisSinifsalFonksiyonTipiClass = class { // ! Çok geliştiginde kullanacaksın.
+
+}
+
+const hataTipiError = new Error("Burası hatadır.") // ! Çok geliştiginde kullanacaksın.
