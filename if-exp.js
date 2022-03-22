c14c91a6c232158858bd58164b86c55c62d826ef
Nurcan Cürebal <nurcancurebal@hotmail.com>
2022-03-22 11:28:17 +0300

    .

--- if-exp.js
+++ if-exp.js
@@ -4,3 +4,31 @@ let ageTwo= 13
 console.log(age >18 ?' You are old enough to drive.' : `You are left with ${18- age} years to drive.`)
 
 console.log(ageTwo >18 ?' You are old enough to drive.' : `You are left with ${18- ageTwo} years to drive.`)
+
+
+
+let myAge= 29
+let yourAge= 92
+if (myAge>yourAge){
+    console.log(`I am ${myAge-yourAge} years older than you`)
+}
+else {
+    console.log(`You are ${yourAge-myAge} years older than me.`)
+}
+
+
+
+let a= 56
+let b=97
+if(a>b){
+    console.log(` ${a} is greater than ${b}`)
+}
+else{
+    console.log(`${b} is greater than ${a}`)
+}
+
+
+
+let one= 9
+let two=45
+console.log(one>two ? `${one} büyüktür ${two}`: `${two} büyüktür ${one}`)
