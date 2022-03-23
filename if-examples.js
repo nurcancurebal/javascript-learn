21da9177116ab3949429c6cc771fab61e89abfc0
Nurcan Cürebal <nurcancurebal@hotmail.com>
2022-03-23 11:15:22 +0300

    .

--- if-examples.js
+++ if-examples.js
@@ -6,3 +6,20 @@ if (day=='monday' || day=='tuesday' || day=='wednesday' || day== 'thursday' || d
 else if(day=='Saturday' || day=='sunday'){
     console.log(`${day} is a weekend.`)
 }
+else {
+    console.log('gün bulunamadı')
+}
+
+
+let days='Monday'
+days= days.toLowerCase()
+switch(days){
+    case "days=='monday' || days=='tuesday' || days=='wednesday' || days== 'thursday' || days== 'friday'":
+        console.log(`${days} is a working day.`)
+        break
+    case "days=='Saturday' || days=='sunday'":
+        console.log(`${days} is a weekend.`)
+        break
+    default:
+        console.log('gün bulunamadı')  //! neden gün bulunamıyor?
+}
