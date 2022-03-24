7424d1caa11204a2560e13f85da595c1da0d409c
Nurcan Cürebal <nurcancurebal@hotmail.com>
2022-03-24 13:57:52 +0300

    .

---
+++ arrayleri-siralama.js
@@ -0,0 +1,12 @@
+
+// sort()
+let numbers= [4,7,2,9,123,236,1]
+numbers.sort()
+console.log(numbers) // sıralma yapıyor ama sıfıncı diziye bakıyor hangisi küçük onu yazıyor 2. diziye bakıyor sıralmayı ona göre yapıyor
+// (7) [1, 123, 2, 236, 4, 7, 9]
+
+let name= ['nurcan', 'ayşe', 'erdinç', 'fatma', 'timur']
+name.sort()
+console.log(name) // (5) ['ayşe', 'erdinç', 'fatma', 'nurcan', 'timur']
+// a' dan z' ye sıralar
+
