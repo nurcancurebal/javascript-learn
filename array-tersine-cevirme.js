57d3141a5f3ca3019da15f900f77a9f6902da876
Nurcan Cürebal <nurcancurebal@hotmail.com>
2022-03-24 13:46:01 +0300

    .

---
+++ array-tersine-cevirme.js
@@ -0,0 +1,9 @@
+const numb=[1,2,3,4,5,6]
+numb.reverse()
+console.log(numb) // (6) [6, 5, 4, 3, 2, 1]
+
+const name='nurcan'
+let newName=name.split('').reverse().join('')
+// önce virgüller ayırarak arraya çevirdi, sonra tersten yazdı,sonra birleştirdi string çevirdi
+console.log(newName)
+console.log(typeof newName)
---
+++ arrayin-basina-ekleme.js
@@ -0,0 +1,7 @@
+let name=['nurcan', 27 , 'erdinç', '42','bal köpüğü']
+
+name.unshift('istanbul')
+// başına eleman ekledim
+name.push('aşk') // sonuna eleman ekledim
+
+console.log(name)
