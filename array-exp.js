2f438417ca6a165f034b116ee2e3809c3bbfaaad
Nurcan Cürebal <nurcancurebal@hotmail.com>
2022-03-25 12:58:04 +0300

    .

---
+++ array-exp.js
@@ -0,0 +1,1 @@
+const shoppingCart = ['Milk', 'Coffee', 'Tea', 'Honey']
---
+++ stringi-arraya-cevir.js
@@ -0,0 +1,13 @@
+//! Exercise lever 2 question 1 yap
+
+
+// split(ayırıcı,limit)
+// split(',',3) virgülden ayırır 3 tane array oluşturur gerisini almaz
+
+let text =
+'I love teaching and empowering people I teach HTML CSS JS React Python'
+console.log(text)
+console.log(text.length)
+let string= text.split(' ')
+console.log(string)
+console.log(string.length)
