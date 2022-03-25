67b3af7b3340a6bf95f2253df75e3c5f4751c36f
Nurcan Cürebal <nurcancurebal@hotmail.com>
2022-03-25 15:00:32 +0300

    .

---
+++ array-eleman-var-mi-ekle.js
@@ -0,0 +1,14 @@
+const webTechs = [
+    'HTML',
+    'CSS',
+    'JavaScript',
+    'React',
+    'Redux',
+    'Node',
+    'MongoDB'
+  ]
+
+  
+
+  let control= webTechs.includes('Sass')
+  console.log(control ?' Sass is a CSS preprocess' : webTechs.push('Sass') && webTechs)
