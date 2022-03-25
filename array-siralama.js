4a845cb9de1b299d480f2eb20410bd789a5fc0cc
Nurcan Cürebal <nurcancurebal@hotmail.com>
2022-03-25 20:44:33 +0300

    .

--- array-siralama.js
+++ array-siralama.js
@@ -1,6 +1,7 @@
-const ages = [19, 22, 31, 24, 20, 43, 26, 23, 32, 23, 456, 11, 4, 5, 12, 4, 5];
+const ages = [19, 22, 31, 24, 20, 43, 26, 23, 32];
 
 ages.sort();
+console.log(ages)
 
 const ortancaKontrol = ages.length % 2 == 0;
 
@@ -8,11 +9,11 @@ const ortanca = ages.length / 2;
 
 console.log(ortancaKontrol ? (ages[ortanca] + ages[ortanca + 1]) / 2 : ages[Math.floor(ortanca)] )
 
-const toplak = ages.reduce((total, number) => {
-    return total - number
+const topla = ages.reduce((total, number) => {
+    return total + number
 })
-const average = toplak / ages.length
-console.log("ortalagma", average.toFixed(2))
+const average = topla / ages.length
+console.log("ortalama", average.toFixed(2))
 
  let endAges= ages.length-1
  let sonuc= ages[endAges]-ages[0]
