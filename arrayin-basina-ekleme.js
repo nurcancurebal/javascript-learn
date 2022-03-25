c1fd73995f947f8e35c6c516a3dc00aa548613ed
Nurcan Cürebal <nurcancurebal@hotmail.com>
2022-03-25 13:51:47 +0300

    .

--- arrayin-basina-ekleme.js
+++ arrayin-basina-ekleme.js
@@ -2,6 +2,8 @@ let name=['nurcan', 27 , 'erdinç', '42','bal köpüğü']
 
 name.unshift('istanbul')
 // başına eleman ekledim
+
+
 name.push('aşk') // sonuna eleman ekledim
 
 console.log(name)
--- arreyden-eleman-cıkarıp-ekleme.js
+++ arreyden-eleman-cıkarıp-ekleme.js
@@ -1,6 +1,6 @@
 const numbers= [1, 2, 3, 4, 5, 6]
 let newNumbers=numbers.splice(1, 4, 'tuğçe', 'nurcan')  //(4) [2, 3, 4, 5] = çıkartılan elemanları buraya yazar
-// başlangıç konumu, çıkartılacak elemanlar, eklenen elemanlar (3 tane değer giriyoruz)
+// başlangıç indeksi, kaç tane eleman silinecek, eklenen elemanlar (3 tane değer giriyoruz)
 
 console.log(numbers) //(4) [1, 'tuğçe', 'nurcan', 6] = son oluşan şeklide sona yazar
 
