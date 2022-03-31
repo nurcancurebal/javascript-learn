cb5cccb4c73c3d56cd1ce6544d54091ea08a8b12
Nurcan Cürebal <nurcancurebal@hotmail.com>
2022-03-31 16:12:02 +0300

    .

--- loops/unique-key-number-generator.js
+++ loops/unique-key-number-generator.js
@@ -5,7 +5,7 @@ while (uniqueKey.length < 5) {
     let randomNumber = Math.floor((Math.random() * 9) + 1);
 
     if (!uniqueKey.includes(randomNumber)) {
-        uniqueKey = uniqueKey + randomNumber;
+        uniqueKey = uniqueKey + randomNumber;  // stringde yan yana yazdırma, string değer olduğu için yan yana yazar
     }
 }
 
