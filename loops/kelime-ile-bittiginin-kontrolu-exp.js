2759df367b2e464df71ecc5b7c8deea32ca45253
Nurcan Cürebal <nurcancurebal@hotmail.com>
2022-03-31 19:09:22 +0300

    .

---
+++ loops/kelime-ile-bittiginin-kontrolu-exp.js
@@ -0,0 +1,27 @@
+const countries = [
+    'Albania',
+    'Bolivia',
+    'Canada',
+    'Denmark',
+    'Ethiopia',
+    'Finland',
+    'Germany',
+    'Hungary',
+    'Ireland',
+    'Japan',
+    'Kenya'
+];
+
+const newCountries = [];
+
+for (i = 0; i < countries.length; i++) {
+
+    if (countries[i].endsWith('ia')) {
+
+        newCountries.push(countries[i]);
+    }
+}
+
+console.log(newCountries);
+
+//! eğer ia ile biten kelime yoksa 'These are countries ends without ia' yazsın
