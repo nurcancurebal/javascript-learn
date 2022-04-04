c375a31e5ca30c60716d89c125e077d3d2b880fd
Nurcan Cürebal <nurcancurebal@hotmail.com>
2022-04-04 17:41:32 +0300

    .

---
+++ loops/en-uzun-kelime-bul-exp.js
@@ -0,0 +1,25 @@
+const webTechs = [
+    'HTML',
+    'CSS',
+    'JavaScript',
+    'React',
+    'Redux',
+    'Node',
+    'MongoDB'
+];
+
+const webTechsLenght = [];
+
+for (let index = 0; index < webTechs.length; index++) {
+
+    webTechsLenght.push(webTechs.length)
+
+    if ( ) {
+
+    } //! koşulu bir türlü yazamadım
+};
+
+
+console.log(webTechs[webTechsLenght])
+
+//TODO  imdatttttt
