519ab8a8ccc478978b24667db89b9beefc8dac56
Nurcan Cürebal <nurcancurebal@hotmail.com>
2022-04-05 19:54:53 +0300

    .

---
+++ functions/functions.js
@@ -0,0 +1,5 @@
+function sayHello(){
+    console.log('Hello!');
+}  // bu kadar kod yazınca çalıştırınca ekrana bir şey gelmez console.log yazmasına rağmen functionu çağırmamız gerekiyor
+
+sayHello()
