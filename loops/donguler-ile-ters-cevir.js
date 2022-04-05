5602932fe96ab35fb517cc3938a4fb853ce0db53
Nurcan Cürebal <nurcancurebal@hotmail.com>
2022-04-05 13:04:02 +0300

    .

---
+++ loops/donguler-ile-ters-cevir.js
@@ -0,0 +1,7 @@
+const fruit= ['banana', 'orange', 'mango', 'lemon'];
+
+let fruitEnd = fruit.length-1;
+
+for (let index = fruitEnd ; index >= 0; index--) {
+    console.log(fruit[index])
+}
---
+++ loops/yan-yana-yazdirma-exp.js
@@ -0,0 +1,16 @@
+const webTechs = [
+    'HTML',
+    'CSS',
+    'JavaScript',
+    'React',
+    'Redux',
+    'Node',
+    'MongoDB'
+];
+
+for (const newWebtechs of webTechs) {
+    console.log(newWebtechs)
+}
+
+
+  //TODO ["HTML", "CSS", "JS", "React", "Redux", "Node", "Express", "MongoDB"]  bu şekilde ekran çıktısı almam gerek for of kullanmam gerek
