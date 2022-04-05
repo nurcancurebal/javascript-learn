b811501a8e04f6bc8f39975a38979cd1e9501e9c
Nurcan Cürebal <nurcancurebal@hotmail.com>
2022-04-05 13:35:12 +0300

    .

---
+++ loops/referanssız-dizi-oluşturmak-exp.js
@@ -0,0 +1,19 @@
+// kopyalama yapmadan yani referanssız dizi oluşturmak
+
+const countries = [
+    'Albania',
+    'Bolivia',
+    'Canada',
+    'Denmark',
+    'Ethiopia',
+    'Finland',
+    'Germany',
+    'Hungary',
+    'Ireland',
+    'Japan',
+    'Kenya'
+  ];
+
+
+  //TODO bu diziden referanssız bir tane daha dizi oluşturup sort metodu ile sıralama yap. sortedCountries in içinde bunu tutun countries dizini sort olmayacak yalnızca sortCountries sıralanmış olacak
+  
