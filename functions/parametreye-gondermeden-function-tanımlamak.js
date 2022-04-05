d8d964be0c9a7c9065c0592385187dd7b4e88ef6
Nurcan Cürebal <nurcancurebal@hotmail.com>
2022-04-05 20:22:25 +0300

    .

---
+++ functions/parametreye-gondermeden-function-tanımlamak.js
@@ -0,0 +1,18 @@
+// function geriye bir şeyler döndürür dolayısıyla parametre alır
+
+let msg = 'merhaba dünya'; // burasıçokönemli functionun içerisindeki ddeeğişken ile dışarıdaki değişken isimleri aynı 
+// olabilir ancak functionun süslü parantezlerinin içi kendi başına bir blok olduğu için blok kapsamında bu yüzden 
+// functiondaki değişkene dışarıdan ulaşılamaz
+
+let name = 'nurcan'
+
+function sayHello() {
+
+    let msg = 'Hello World';
+    console.log(msg , name);  // dışarıdaki bir değişkene functionun içinde müdahale edilebilir ancak functionun içindeki bir değişkene dışarıdan müdehala edilemez
+}
+
+sayHello(); // functiondaki msg değişkenini çağırdık
+
+
+// geriye her hangi bir şey döndürmediğimiz zaman functionlar " undefined " döndürür
