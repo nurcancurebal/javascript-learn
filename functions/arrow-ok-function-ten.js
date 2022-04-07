bc7e93a61723b2af8226fd89d49abe450bfb6219
Nurcan Cürebal <nurcancurebal@hotmail.com>
2022-04-07 19:31:06 +0300

    .

--- functions/arrow-ok-function-ten.js
+++ functions/arrow-ok-function-ten.js
@@ -1,1 +1,34 @@
 // fonksiyon yazmanın bir alternatifi, arrow= ok fonksiyonu denir
+
+
+// normal function(regular function)
+
+function hello(name) {
+
+    return `Merhaba ${name}`;
+
+};
+
+console.log(hello('Nurcan'));
+
+
+// arrow (ok) function 
+
+const hello2 = name => `Merhaba ${name}`;  // tek bir değişken varsa paranteze gerek yok ve tek satırlık bir işlem ise returnve süslü parantez yazmaya gerek yok
+
+console.log(hello2('Erdinç'));
+
+
+function hello3(name, surname) {
+
+    return `Merhaba ${name} ${surname}`;
+
+};
+
+console.log(hello3('Nurcan', 'cürebal'));
+
+
+
+const hello4 = (name, surname) => `Merhaba ${name} ${surname}`;
+
+console.log(hello4('Erdinç', 'cürebalycy'));
--- functions/function-two.js
+++ functions/function-two.js
@@ -1,7 +1,7 @@
 // parametreye gondermeden function tanımlamak
 // function geriye bir şeyler döndürür dolayısıyla parametre alır
 
-let msg = 'merhaba dünya'; // burasıçokönemli functionun içerisindeki ddeeğişken ile dışarıdaki değişken isimleri aynı 
+let msg = 'merhaba dünya'; // NOT: Functionun içerisindeki ddeeğişken ile dışarıdaki değişken isimleri aynı 
 // olabilir ancak functionun süslü parantezlerinin içi kendi başına bir blok olduğu için blok kapsamında bu yüzden 
 // functiondaki değişkene dışarıdan ulaşılamaz
 
