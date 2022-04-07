06b6a18f0ea50cd7b128630b50d431fa5dc04f00
Nurcan Cürebal <nurcancurebal@hotmail.com>
2022-04-07 13:28:48 +0300

    .

---
+++ functions/function-array-five.js
@@ -0,0 +1,16 @@
+function sumArrayValues(array) {
+
+    let total = 0;
+
+    for (let i = 0; i < array.length; i++) {
+
+        total += array[i];
+
+    };
+    return total;
+
+};
+
+let dizi = [2, 4, 6, 8, 10];
+
+console.log('toplam', sumArrayValues[dizi]);
--- functions/parametrelerle-function-four.js
+++ functions/parametrelerle-function-four.js
@@ -1,10 +1,10 @@
 // functionun içine farklı veri türlerinde parametreler gönderebilirsin
 
-function multiplay(num1, num2){
+function multiplay(num1, num2) {
 
-    return num1*num2  // burada return olmadığı zaman undefined döndürüyor
+    return num1 * num2  // burada return olmadığı zaman undefined döndürüyor
 }
 
-let total= multiplay(2, 5);
+let total = multiplay(2, 5);
 
-console.log('çarpım', total)
+console.log('çarpım', total);
