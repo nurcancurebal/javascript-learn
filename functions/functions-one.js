0680b68fbaf31d9a12bd2652cb49cbad50cbfa81
Nurcan Cürebal <nurcancurebal@hotmail.com>
2022-04-07 12:50:03 +0300

    .

--- functions/functions-one.js
+++ functions/functions-one.js
@@ -3,3 +3,9 @@ function sayHello(){
 }  // bu kadar kod yazınca çalıştırınca ekrana bir şey gelmez console.log yazmasına rağmen functionu çağırmamız gerekiyor
 
 sayHello()
+
+
+/* function
+1. temiz ve okunması kolay = genelde tek bir şey yapmayı amaçlar
+2. yeniden kullanılabilir 
+3. test edilmesi kolay */
--- functions/parametrelerle-function-four.js
+++ functions/parametrelerle-function-four.js
@@ -1,1 +1,10 @@
-// 9.49   28.38
+// functionun içine farklı veri türlerinde parametreler gönderebilirsin
+
+function multiplay(num1, num2){
+
+    return num1*num2  // burada return olmadığı zaman undefined döndürüyor
+}
+
+let total= multiplay(2, 5);
+
+console.log('çarpım', total)
