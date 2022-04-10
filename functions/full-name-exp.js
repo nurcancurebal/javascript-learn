436d810e582a67404760926f817e2b2173423ca4
Nurcan Cürebal <nurcancurebal@hotmail.com>
2022-04-10 22:25:17 +0300

    .

--- functions/full-name-exp.js
+++ functions/full-name-exp.js
@@ -1,3 +1,35 @@
-function fullName(firsName, lastName) {
-    
+function printFullName(firstName, lastName) {
+
+    let space = ' '
+    let fullname = firstName + space + lastName;
+
+    return fullname;
 };
+console.log(printFullName('Nurcan', 'Cürebal'));
+
+
+// ya da
+
+
+function printFullNameTwo() {
+
+    let name = 'Erdinç';
+    let surname = 'Cürebal';
+    let spaceTwo = ' ';
+    let fullNameTwo = name + spaceTwo + surname;
+    return fullNameTwo;
+};
+
+console.log(printFullNameTwo());
+
+
+// ya da
+
+
+
+function printFullNameThree() {
+
+    console.log('Bal'+' '+'Cürebal');
+};
+
+printFullNameThree();
