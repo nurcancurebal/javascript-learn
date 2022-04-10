ab91f9f293a59c8d15417b2a67fde5fe032f8107
Nurcan Cürebal <nurcancurebal@hotmail.com>
2022-04-10 19:46:58 +0300

    .

---
+++ functions/tek-satirda-dondurme.js
@@ -0,0 +1,9 @@
+const fullName = (firstName, secondName) => {
+    return `${firstName} ${secondName}`
+};
+console.log(fullName('nurcan', 'cürebal'));
+console.log(fullName('erdinç', 'cürebal'));
+
+
+const fullNameTwo = (first, last) => `${first} ${last}`;
+console.log(fullNameTwo('bal','cürebal'))
