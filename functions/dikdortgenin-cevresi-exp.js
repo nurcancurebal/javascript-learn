1fcce18b94e604e489c923e48218988f8ba8768a
Nurcan Cürebal <nurcancurebal@hotmail.com>
2022-04-19 17:05:05 +0300

    .

---
+++ functions/dikdortgenin-cevresi-exp.js
@@ -0,0 +1,7 @@
+function perimeterOfRectangle(length, width) {
+
+    let perimeter = 2 * (length + width);
+    return perimeter;
+};
+
+console.log(`Dikdörtgenin çevresi = ${perimeterOfRectangle(2, 5)}`);
