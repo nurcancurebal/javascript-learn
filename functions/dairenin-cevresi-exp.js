4553b91700e76471d73d42be491dd8051fbd4567
Nurcan Cürebal <nurcancurebal@hotmail.com>
2022-04-19 19:03:32 +0300

    .

---
+++ functions/dairenin-cevresi-exp.js
@@ -0,0 +1,9 @@
+function circumOfCircle(r) {
+
+    let circumference = 2 * 3,14 * r;
+    return circumference;
+};
+
+console.log(`Dairenin çevresi= ${circumOfCircle(8)}`);
+
+//TODO neden hata veriyor
