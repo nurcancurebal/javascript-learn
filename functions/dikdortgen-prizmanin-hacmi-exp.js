7477f43f2346a1e1c456d1f2ba8905008e20c322
Nurcan Cürebal <nurcancurebal@hotmail.com>
2022-04-19 17:38:40 +0300

    .

---
+++ functions/dikdortgen-prizmanin-hacmi-exp.js
@@ -0,0 +1,9 @@
+function volumeOfRectPrism(length, width, height) {
+
+    let volume = length*width*height;
+    return volume;
+};
+
+let finalValue = volumeOfRectPrism(5,4,3);
+
+console.log(`Dikdörtgen prizmanın hacmi= ${finalValue}`);
