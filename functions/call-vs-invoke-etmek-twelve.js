70fcf90b6207d10073ab4f8c11383b2f5efec0a4
Nurcan Cürebal <nurcancurebal@hotmail.com>
2022-04-19 13:58:01 +0300

    .

--- functions/call-vs-invoke-etmek-twelve.js
+++ functions/call-vs-invoke-etmek-twelve.js
@@ -1,14 +1,14 @@
-function test(){
+function test() {
+
     console.log('test fonksiyonu çalıştı!')
 };
-
 test();   // call etmek ( doğrudan çağırma )
 
 
 
 
-
 function invoker(func) {
+    
     func();
 };
 invoker(test);  // invoke etmek( dolaylı yoldan çağırma )
