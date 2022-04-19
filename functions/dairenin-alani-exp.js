d8efa3c1fadf5120de77964625f2121a54296c29
Nurcan Cürebal <nurcancurebal@hotmail.com>
2022-04-19 20:55:12 +0300

    .

--- functions/dairenin-alani-exp.js
+++ functions/dairenin-alani-exp.js
@@ -1,8 +1,6 @@
 function circle(r) {
 
-    console.log( r*r*3,14);
+    console.log( r*r*3.14);
     
 };
 circle(5);
-
-//TODO sonuç 78,5 neden farklı çıktı
--- functions/dairenin-cevresi-exp.js
+++ functions/dairenin-cevresi-exp.js
@@ -1,9 +1,7 @@
 function circumOfCircle(r) {
 
-    let circumference = 2 * 3,14 * r;
+    let circumference = 2 * 3.14 * r;
     return circumference;
 };
 
 console.log(`Dairenin çevresi= ${circumOfCircle(8)}`);
-
-//TODO neden hata veriyor
