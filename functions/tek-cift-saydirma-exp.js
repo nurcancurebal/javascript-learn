e962bc29ffde489625502b76552432e5d627f72d
Nurcan Cürebal <nurcancurebal@hotmail.com>
2022-04-20 15:43:18 +0300

    .

---
+++ functions/tek-cift-saydirma-exp.js
@@ -0,0 +1,30 @@
+function evensAndOdds(sayi1, sayi2 = 0) {
+
+    let evensTotal = 0;
+    let oddsTotal = 0;
+
+    for (let index = 0; index < sayi1; index++) {
+
+        if (sayi2 % 2 == 0) {
+
+            evensTotal += sayi2;
+
+        } else {
+
+            oddsTotal += sayi2;
+        };
+
+        sayi2 = sayi2 + 1;
+    };
+
+    return evensTotal, oddsTotal;
+
+};
+
+evensAndOdds(100);
+
+console.log(`The number of evens are ${evensTotal}`);
+console.log(`The number of odds are ${oddsTotal}`);
+
+
+//TODO neden çalışmadı
