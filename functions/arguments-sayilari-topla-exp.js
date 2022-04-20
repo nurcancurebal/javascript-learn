75efd10aab8d5ee0fb5122e0aaab4d4f0d17c10f
Nurcan Cürebal <nurcancurebal@hotmail.com>
2022-04-20 16:10:59 +0300

    .

--- functions/arguments-sayilari-topla-exp.js
+++ functions/arguments-sayilari-topla-exp.js
@@ -1,7 +1,12 @@
 function topla() {
-    
+
+    let total = 0;
+
     for (let index = 0; index < arguments.length; index++) {
-    
-        
+
+        total += arguments[index];
     };
+    return total;
 };
+
+console.log(topla(3, 5, 10));
