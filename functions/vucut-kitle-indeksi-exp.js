2847d9dc04b682ca8c38067ede4c496f22887ec1
Nurcan Cürebal <nurcancurebal@hotmail.com>
2022-04-20 18:35:27 +0300

    .

--- functions/vucut-kitle-indeksi-exp.js
+++ functions/vucut-kitle-indeksi-exp.js
@@ -4,4 +4,27 @@ function bodyMassIndex(weight, height) {
     return bmi;
 };
 
-console.log(`Vücut kitle indeksi= ${bodyMassIndex(55, 1.60)}`);
+let sonuc = bodyMassIndex(-150, 1.60);
+
+console.log(`Vücut kitle indeksi= ${sonuc}`);
+
+if (0 <= sonuc && sonuc <= 18.4) {
+
+    console.log(`Underweight: BMI is less than 18.5`);
+
+} else if (18.5 <= sonuc && sonuc <= 24.9) {
+
+    console.log(`Normal weight: BMI is 18.5 to 24.9`);
+
+} else if (25 <= sonuc && sonuc <= 29.9) {
+
+    console.log(`Overweight: BMI is 25 to 29.9`);
+
+} else if (sonuc >= 30) {
+
+    console.log(`Overweight: BMI is 25 to 29.9`);
+
+} else {
+
+    console.log('Tam sayı girmediniz')
+};
--- if-else-switch-ternary/if-switch.js
+++ if-else-switch-ternary/if-switch.js
@@ -3,6 +3,7 @@ if (num>0){
     console.log(`${num} is a pozitif number` )
 } // num değişkeni 0 dan küçük olur ise çalışmaz, her hangi bir çıktı vermez
 
+
 let num2= -2
 if (num2>0){
     console.log(`${num2} is a pozitif number `)
