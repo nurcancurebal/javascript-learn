068da8d351681d50860552e20d7dcb427644ca6e
Nurcan Cürebal <nurcancurebal@hotmail.com>
2022-04-21 13:23:40 +0300

    .

--- functions/ikinci-dereceden-denklem-exp.js
+++ functions/ikinci-dereceden-denklem-exp.js
@@ -1,3 +1,26 @@
-function solveQuadEquation(a,b,c) {
-    
-}
+function solveQuadEquation(a, b, c) {
+
+    let delta = (b ** 2) + (-4 * a * c);
+
+    if (delta > 0) {
+
+        let x1 = (-b + (Math.sqrt(delta))) / 2 * a;
+        let x2 = (-b - (Math.sqrt(delta))) / 2 * a;
+
+        console.log(`Çözüm kümesi iki reel kök var x1= ${x1}, x2= ${x2}`);
+
+    } else if (delta == 0) {
+
+        let x3 = -b / (2 * a);
+
+        console.log(`Çözüm kümesi çakışık kök var x1=x2= ${x3}`);
+
+    } else {
+
+        console.log('Boş küme, kök yok');
+    };
+};
+
+solveQuadEquation(1, 5, -14); // Çözüm kümesi iki reel kök var x1= 2, x2= -7
+// solveQuadEquation(9, -6, 1);  // Çözüm kümesi çakışık kök var x1=x2= 0.3333333333333333
+// solveQuadEquation(1, 1, 2);   // Boş küme, kök yok
