b46c7289263e54ab822dab5f35edda5002bf6638
Nurcan Cürebal <nurcancurebal@hotmail.com>
2022-04-21 17:06:51 +0300

    .

--- functions/en-buyugu-bulma-math-max-exp.js
+++ functions/en-buyugu-bulma-math-max-exp.js
@@ -1,6 +1,12 @@
-function findMax(num1, num2, num3) {
+function findMaxMin(num1, num2, num3) {
 
-    return Math.max(num1, num2, num3);
-
+     let max = Math.max(num1, num2, num3);
+     let min = Math.min(num1, num2, num3);
+     return [max,min];
 };
-console.log(findMax(15, 777, -10));
+
+console.log(findMaxMin(0, 10, 5));
+console.log(findMaxMin(0, -10, -2));
+
+
+//TODO birinci çıktı maximumu, ikinci çıktı minimumu verecek ve çıktı array olmayacak
