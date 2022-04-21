4a302f420a6bbcfe1754eb61908f600023e43f3d
Nurcan Cürebal <nurcancurebal@hotmail.com>
2022-04-21 13:48:21 +0300

    .

---
+++ functions/array-olusturma-exp.js
@@ -0,0 +1,14 @@
+function printArray() {
+
+    for (arguments of Object.entries(arguments)) {
+        
+    }
+   console.log(arguments);
+
+};
+
+printArray(3,4,5);
+
+// parametre olarak array alır, ve array olarak yazdırır
+
+//TODO ne demek anlamadım
