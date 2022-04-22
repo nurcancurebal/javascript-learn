27df1cd04d998a8126f365ccc71ccc32e87b05bc
Nurcan Cürebal <nurcancurebal@hotmail.com>
2022-04-22 20:48:04 +0300

    .

--- functions/index-parametresi-exp.js
+++ functions/index-parametresi-exp.js
@@ -1,1 +1,19 @@
-// TODO function name removeItem indeks parametresi alır ve  bir öğeyi çıkardıktan sonra bir array döndürür
+// indeks parametresi alır ve  bir öğeyi çıkardıktan sonra bir array döndürür,
+
+const array = ["Bursa", "Antalya", "İzmir", "Ankara"];
+
+
+console.log(removeItem(array, 2));
+
+function removeItem(funcArray, index) {
+
+    const newArray = [];
+
+    for (let index = 0; index < funcArray.length; index++) {
+        newArray.push(funcArray[index]);
+    };
+
+    newArray.splice(index, 1);
+
+    return newArray;
+};
--- functions/item-parametresi-exp.js
+++ functions/item-parametresi-exp.js
@@ -1,1 +1,20 @@
-//TODO level 2 soru 8  function adı addItem. bir item parametresi alır ve itemi ekledikten sonra bir array döndürür
+// bir item parametresi alır ve itemi ekledikten sonra bir array döndürür
+
+const array = ["Bursa", "Antalya", "İzmir", "Ankara"];
+
+console.log(addItem(array, "Bodrum", "Kıbrıs"));
+
+function addItem(funcArray, ...arg) {
+
+    const newArray = [];
+
+    for (let index = 0; index < funcArray.length; index++) {
+        newArray.push(funcArray[index]);
+    };
+
+    for (let index = 0; index < arg.length; index++) {
+        newArray.push(arg[index]);
+    };
+
+    return newArray;
+};
