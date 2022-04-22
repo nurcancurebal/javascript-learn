8460c5ca4303827ecc22d4a3f6997c7c22bef3e6
Nurcan Cürebal <nurcancurebal@hotmail.com>
2022-04-22 22:04:04 +0300

    .

--- functions/random-mac-address-exp.js
+++ functions/random-mac-address-exp.js
@@ -1,36 +1,36 @@
 
 const macAddress = macAddressGenerator(":");
 
-console.log(macAddress)
+console.log(macAddress);
 
 
 function macAddressGenerator(splitOperator) {
 
-    // ? Tekil hexler.
+    // Tekil hexler.
     const array = [];
 
     for (let index = 0; index < 6; index++) {
-        array.push(hexNumberGenerator());     
-    }   
+        array.push(hexNumberGenerator());
+    };
 
-    // ? Dizindeki arrayleri belirli ifade ile stringe cevirir.
-    const mac = array.join(splitOperator)
+    // Dizindeki arrayleri belirli ifade ile stringe cevirir.
+    const mac = array.join(splitOperator);
 
-    return mac
-}
+    return mac;
+};
 
 
 function hexNumberGenerator() {
 
-    // ? 17 ile 255 arası sayı üretir.
+    // 17 ile 255 arası sayı üretir.
     const numberGenerator = Math.floor((Math.random() * 239) + 17);
 
-    // ? Hexedecimal sayıya cevirir.
+    // Hexedecimal sayıya cevirir.
     const convertHexa = Number(numberGenerator).toString(16);
 
-    // ? Veriyi upper case yap.
+    // Veriyi upper case yap.
     const hexToUpperCase = String(convertHexa).toUpperCase();
 
-    // ? Veriyi fonksiyondan dışarı cıkart.
+    // Veriyi fonksiyondan dışarı cıkart.
     return hexToUpperCase;
-}
+};
