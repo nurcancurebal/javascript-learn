35afb0a2d52bc0feb8d708e77d35820eea8d7ff1
Nurcan Cürebal <nurcancurebal@hotmail.com>
2022-04-25 15:06:52 +0300

    .

--- functions/random-hexadecimal-mac-address-exp.js
+++ functions/random-hexadecimal-mac-address-exp.js
@@ -13,23 +13,27 @@ function macAddressGenerator(splitOperator) {
     };
 
     // Dizindeki arrayleri belirli ifade ile stringe cevirir.
+    // splitOPerator dediğimiz = ':' oluyor. yani join splitOperator'u kullanarak birleştir demek
     const mac = array.join(splitOperator);
 
     return mac;
 };
 
+// console.log(macAddressGenerator()); //9A,1E,6D,A5,D8,8D
+
 
 function hexNumberGenerator() {
 
-    // 17 ile 255 arası sayı üretir. çünkü hexada rakamlar 16 ya kadardır (0,1,2,3,4,5,6,7,8,9,A,B,C,D,E,F); 255=ff, 256=ff1 yani  256 3 karakter üretir
+    // 17 ile 255 arası sayı üretir. çünkü hexa da rakamlar 16 ya kadardır (0,1,2,3,4,5,6,7,8,9,A,B,C,D,E,F); 255=ff, 256=ff1 yani  256 3 karakter üretir
     const numberGenerator = Math.floor((Math.random() * 239) + 17);
 
     // Hexedecimal sayıya cevirir.
     const convertHexa = Number(numberGenerator).toString(16);
 
-    // Veriyi upper case yap.
+    // Verinin içindeki harfleri büyüttük
     const hexToUpperCase = String(convertHexa).toUpperCase();
 
     // Veriyi fonksiyondan dışarı cıkart.
     return hexToUpperCase;
 };
+ // hexNumberGenerator() = iki haneli hexa number oluşturdu
