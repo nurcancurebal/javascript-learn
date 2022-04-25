6bd140062669a67ce41a37244fd1f1628a692155
Nurcan Cürebal <nurcancurebal@hotmail.com>
2022-04-25 18:02:35 +0300

    .

--- functions/random-array-hexadecimal-exp.js
+++ functions/random-array-hexadecimal-exp.js
@@ -1,22 +1,22 @@
 function arrayOfHexaColors(sayi) {
-    
+
     for (let index = 0; index < sayi; index++) {
 
         const newArray = [];
-    const randomNumbers = [];
+        const randomNumbers = [];
 
-    for (let index = 0; index < 5; index++) {
+        for (let index = 0; index < 5; index++) {
 
-        const numberGenerator = Math.floor((Math.random() * 100) + 1);
-        randomNumbers.push(numberGenerator);
-    };
+            const numberGenerator = Math.floor((Math.random() * 100) + 1);
+            randomNumbers.push(numberGenerator);
+        };
 
-    let unite = randomNumbers.join('');
-    let convertHexa = Number(unite).toString(16);
-    newArray.push(convertHexa)
+        let unite = randomNumbers.join('');
+        let convertHexa = Number(unite).toString(16);
+        newArray.push(convertHexa);
 
-    return newArray;
-        
+        return newArray;
+
     };
 };
 
