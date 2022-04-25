75fb117033cb83fb38b5cfd4b4a0a649b5ddc877
Nurcan Cürebal <nurcancurebal@hotmail.com>
2022-04-25 17:30:49 +0300

    .

---
+++ functions/random-rgb-colors-exp.js
@@ -0,0 +1,14 @@
+
+
+function rgbColorGenerator() {
+
+    const rgbColors = [];
+
+    for (let index = 0; index < 3; index++) {
+
+        rgbColors.push(Math.floor((Math.random() * 255) + 1));
+    };
+    console.log(`rgb(${rgbColors[0]},${rgbColors[1]},${rgbColors[2]})`);
+};
+
+rgbColorGenerator()
