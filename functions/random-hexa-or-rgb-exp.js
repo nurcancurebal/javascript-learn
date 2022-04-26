28a638e4de03da3aa3b65f9c1e2ea175397a696b
Nurcan Cürebal <nurcancurebal@hotmail.com>
2022-04-26 18:17:29 +0300

    .

--- functions/random-hexa-or-rgb-exp.js
+++ functions/random-hexa-or-rgb-exp.js
@@ -1,31 +1,49 @@
-// generateColors('hexa', 3);
- // generateColors('hexa', 1);
- generateColors('rgb', 3);
+// generateColors('hexa', 5);
+// generateColors('hexa', 1);
+generateColors('rgb', 2);
 // generateColors('rgb', 1);
 
 function generateColors(hexaRgb, number) {
 
-
     if (hexaRgb == 'hexa') {
 
         const array = [];
+        const arrayDiyez = [];
+        const endArray = [];
+
+        for (let index = 0; index < 5; index++) {
+
+            const numberGenerator = Math.floor((Math.random() * 100) + 1);
+            array.push(numberGenerator);
+        };
+
+        const unit = array.join('');
+        const convertHexa = Number(unit).toString(16);
+        let diyez = '#' + convertHexa;
+        arrayDiyez.push(diyez);
 
         for (let index = 0; index < number; index++) {
 
-            const numberGenerator = Math.floor((Math.random() * 10000) + 1);
-            const convertHexa = Number(numberGenerator).toString(16);
-            let diyez = '#' + convertHexa;
-            array.push(diyez);
+            endArray.push(arrayDiyez);
         };
-        console.log(array);
-    } else {
+
+        console.log(endArray);
+
+    } else if (hexaRgb == 'rgb') {
 
         const rgbColors = [];
 
-        for (let index = 0; index < number; index++) {
+        for (let index = 0; index < 3; index++) {
 
             rgbColors.push(Math.floor((Math.random() * 255) + 1));
         };
-        console.log([`rgb(${arrayOfRgbColors()})`]);
+
+        for (let index = 0; index < number; index++) {
+
+            console.log([`rgb(${rgbColors})`]);
+        };
     };
 };
+
+
+// TODO level 3 soru 7
