785fc9215fd952399d81565f9fdc0815fcd8ed6f
Nurcan Cürebal <nurcancurebal@hotmail.com>
2022-04-26 23:46:19 +0300

    .

--- functions/user-id-generated-by-user-exp.js
+++ functions/user-id-generated-by-user-exp.js
@@ -1,6 +1,3 @@
-// userIdGenerator fonksiyonunu değiştirin userIdGeneratedByUser bir fonksiyon adı bildirin. Herhangi bir parametre almaz, ancak komut ()
-// kullanarak iki girdi alır. Girdilerden biri karakter sayısıdır ve ikinci girdi, üretilmesi gereken kimlik sayısıdır.
-
 const data = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "a", "B", "c", "D", "e", "F", "g", "H", "i", "J", "k", "L", "m", "N", "o", "P", "q", "R", "s", "T", "u", "V", "w", "X", "y", "Z",];
 
 
@@ -31,4 +28,3 @@ function userIdGeneratedByUser() {
 
     return randomKey;
 };
-//TODO
