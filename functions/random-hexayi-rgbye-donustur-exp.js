442492965706345d59560cbcb304dc4b114e81b9
Nurcan Cürebal <nurcancurebal@hotmail.com>
2022-04-26 14:21:01 +0300

    .

--- functions/random-hexayi-rgbye-donustur-exp.js
+++ functions/random-hexayi-rgbye-donustur-exp.js
@@ -1,3 +1,19 @@
+console.log(convertHexaToRgb());
+
 function convertHexaToRgb() {
-    
-}
+
+    const randomNumbers = [];
+
+    for (let index = 0; index < 5; index++) {
+
+        const numberGenerator = Math.floor((Math.random() * 100) + 1);
+        randomNumbers.push(numberGenerator);
+    };
+
+    let unite = randomNumbers.join('');     // sayıları birleştirip string yaptık
+    let convertHexa = Number(unite).toString(16);   // stringi numbera çevirip, hexa ya çevirdik
+
+    return convertHexa;
+};
+
+// TODO Hexa rengini rgb'ye dönüştüren ve bir rgb rengi döndüren convertHexaToRgb işlevini yazın
