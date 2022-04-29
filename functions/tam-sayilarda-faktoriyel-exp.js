3ecc0a93d8ff06eebd14b15f32c1e4638110ae32
Nurcan Cürebal <nurcancurebal@hotmail.com>
2022-04-29 17:46:37 +0300

    .

---
+++ functions/tam-sayilarda-faktoriyel-exp.js
@@ -0,0 +1,31 @@
+// Fonksiyonunuzu faktöriyel olarak adlandırın, parametre olarak bir tam sayı alır ve sayının faktöriyelini döndürür.
+
+factorial(-1);
+factorial(10);
+factorial(3);
+factorial(-3);
+
+function factorial(value) {
+
+    if (value > 0) {
+
+        let sumValue = 1;
+
+        for (let index = 1; index <= value; index++) {
+
+            sumValue = sumValue * index;
+        };
+        console.log(`faktöriyel = ${sumValue}`);
+
+    } else if (value < 0) {
+
+        let sumValue = 1;
+
+        for (let index = value; index < 0; index++) {
+
+            sumValue = sumValue * index;
+        };
+
+        console.log(`faktöriyel = ${sumValue}`);
+    };
+};
