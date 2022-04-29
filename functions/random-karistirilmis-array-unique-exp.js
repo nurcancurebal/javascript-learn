ed45ebf0eddf0590fdf0c16c7a4e84d72183cc51
Nurcan Cürebal <nurcancurebal@hotmail.com>
2022-04-29 17:14:56 +0300

    .

--- functions/random-karistirilmis-array-unique-exp.js
+++ functions/random-karistirilmis-array-unique-exp.js
@@ -1,18 +1,14 @@
-// TODO Fonksiyonunuzu shuffleArray olarak adlandırın, parametre olarak bir array alır ve karıştırılmış bir array döndürür
+// Fonksiyonunuzu shuffleArray olarak adlandırın, parametre olarak bir array alır ve karıştırılmış bir array döndürür
 
-
-let dizi = [12, 2, 4, 4,8, 6, 8, 10];
+let dizi = [12, 2, 4, 4, 8, 6, 8, 10];
 
 const result = shuffleArray(dizi);
 
 console.log(result);
 
-
 function shuffleArray(array) {
 
+    array.sort(() => Math.random() - 0.5); // arrayleri karıştırmak için kısa bir yoldur ! özel bir koddur
 
-    array.sort(() => Math.random() - 0.5) // arrayleri karıştırmak için kısa bir yoldur
-
-    return array
-
+    return array;
 };
