1090585d018dd5c2e65d17a4b675092cebee66af
Nurcan Cürebal <nurcancurebal@hotmail.com>
2022-04-29 19:47:39 +0300

    .

---
+++ functions/bos-olup-olmadigi-exp.js
@@ -0,0 +1,16 @@
+isEmpty([3546758]);
+isEmpty(`fghjk`);
+isEmpty('dfghj');
+isEmpty();
+isEmpty('');
+isEmpty(``);
+
+
+function isEmpty(params) {
+
+    if (!params) {
+        console.log('Parametre yok!');
+    } else {
+        console.log('Parametre var!');
+    };
+};
