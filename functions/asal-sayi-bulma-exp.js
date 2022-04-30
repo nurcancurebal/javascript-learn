fb7afbe00ef48ae1891834161febcbc8bde5fdfc
Nurcan Cürebal <nurcancurebal@hotmail.com>
2022-04-30 22:25:56 +0300

    .

--- functions/asal-sayi-bulma-exp.js
+++ functions/asal-sayi-bulma-exp.js
@@ -1,18 +1,29 @@
-console.log(isPrime(3, 5, 7, 9, 10, 11, 12, 13, 14));
+isPrime(3, 5, 7, 9, 10, 11, 12, 13, 14, 15, 16, 17);
 
-function isPrime(prime) {
+function isPrime() {
 
-    if (prime < 0) {
+    for (let i = 0; i < arguments.length; i++) {
 
-        console.log('Girilen sayı negatiftir, asal olamaz.');
+        if (arguments[i] < 0) {
 
-    } else {
+            console.log('Girilen sayı negatiftir, asal olamaz.');
 
-        for (let index = 0; index < prime.length; index++) {
-            
-            prime/index
-        }
+        } else {
 
-    }
+            let control = 0;
 
+            for (let index = 1; index <= arguments[i]; index++) {
+
+                if (arguments[i] % index == 0) {
+
+                    control++;
+                };
+            };
+
+            if (control == 2) {
+
+                console.log(`Sayı asal sayıdır ${arguments[i]}`);
+            };
+        };
+    };
 };
