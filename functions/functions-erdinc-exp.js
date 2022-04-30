d664ce408a2977dfb06c4da4d8a6fd14f984a769
Nurcan Cürebal <nurcancurebal@hotmail.com>
2022-04-30 21:42:28 +0300

    .

--- functions/functions-erdinc-exp.js
+++ functions/functions-erdinc-exp.js
@@ -4,26 +4,28 @@ const c = 7;
 
 const jj = c;
 
-const cikarma = function (deger1, deger2, deger3){  // anonim fonksiyon
+const cikarma = function (deger1, deger2, deger3) {  // anonim fonksiyon
+
     const cikar = deger1 - deger2 - deger3;
     return cikar;
-}
+};
 
-const toplananlar = toplama(5,6,7);
-const cikarilanlar = cikarma(5,6,7);
+const toplananlar = toplama(5, 6, 7);
+const cikarilanlar = cikarma(5, 6, 7);
 
 console.log(toplananlar);
 console.log(cikarilanlar);
 
-function toplama(deger1, deger2, deger3){
+function toplama(deger1, deger2, deger3) {
+
     const topla = deger1 + deger2 + deger3;
     return topla;
-}
+};
 
-const sehirler = ["ankara","istanbul","bursa","izmir"];
+const sehirler = ["ankara", "istanbul", "bursa", "izmir"];
 
-sehirler.forEach(function (item, index){
-    console.log(index, item)
-    console.log(item == "istanbul")
-})
+sehirler.forEach(function (item, index) {
 
+    console.log(index, item);
+    console.log(item == "istanbul");
+});
