5a76ade201bce20dfc59f47e091dd932404f7a17
Nurcan Cürebal <nurcancurebal@hotmail.com>
2022-05-01 17:03:21 +0300

    .

---
+++ functions/arrayi-kopyala-tersine-dondur-exp.js
@@ -0,0 +1,30 @@
+const countries = [
+    'Albania',
+    'Bolivia',
+    'Canada',
+    'Denmark',
+    'Ethiopia',
+    'Finland',
+    'Germany',
+    'Hungary',
+    'Ireland',
+    'Japan',
+    'Kenya'];
+
+
+console.log(reverseCountries(countries));
+
+console.log(countries);
+
+function reverseCountries(country) {
+
+    const duplicateCountries = []; // arrayi kopyaladık
+
+    for (let index = 0; index < countries.length; index++) {
+
+        duplicateCountries.push(country[index]);
+    };
+    duplicateCountries.reverse();
+
+    return duplicateCountries;
+};
--- functions/degiskenlerin-gecerliligi-exp.js
+++ functions/degiskenlerin-gecerliligi-exp.js
@@ -1,10 +1,35 @@
 // JavaScript değişken adı, $ veya _ dışında özel karakterleri veya sembolleri desteklemez. Bir değişkenin geçerli
-// veya geçersiz değişken olup olmadığını kontrol eden bir isValidVariable işlevi yazın.
+// veya geçersiz değişken olup olmadığını kontrol eden bir isValidVariable fonksiyonu yazın.
+
+const WRITE_CHAR_LETTER = ["q", "w", "e", "r", "t", "y", "u", "o", "p", "a", "s", "d", "f", "g", "h", "j", "k", "l", "i", "z", "x", "c", "v", "b", "n", "m", "Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P", "A", "S", "D", "F", "G", "H", "J", "K", "L", "Z", "X", "C", "V", "B", "N", "M"]
+const WRITE_CHAR_NUMBER = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+const WRITE_CHAR_SPECIAL = ["_", "$"];
 
 
-function isValidVariable() {
+const varible = "aasd123as";
 
+
+const varibleStatus = isValidVariable(varible);
+
+console.log(varibleStatus)
+
+function isValidVariable(value) {
+
+
+    const typeOne = [...WRITE_CHAR_LETTER, ...WRITE_CHAR_SPECIAL]; // ... üç nokta dizini dışarıya cıkartmaya yarar.
     
-};
+    if (!typeOne.includes(value[0])) {
+        return "Geçersiz değişkendir."
+    }
 
-//TODO
+    const typeTwo = [...WRITE_CHAR_LETTER, ...WRITE_CHAR_SPECIAL, ...WRITE_CHAR_NUMBER];
+    for (let index = 0; index < value.length; index++) {
+
+        if (!typeTwo.includes(value[index])) {
+            return "Geçersiz değişkendir."
+        }
+
+    }
+
+    return "Geçerli değişkendir.";
+};
