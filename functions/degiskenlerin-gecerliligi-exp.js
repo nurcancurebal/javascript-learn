0efd056a3d08642ce681f06b7595387a317c6fb2
Nurcan Cürebal <nurcancurebal@hotmail.com>
2022-05-01 13:05:13 +0300

    .

---
+++ functions/degiskenlerin-gecerliligi-exp.js
@@ -0,0 +1,10 @@
+// JavaScript değişken adı, $ veya _ dışında özel karakterleri veya sembolleri desteklemez. Bir değişkenin geçerli
+// veya geçersiz değişken olup olmadığını kontrol eden bir isValidVariable işlevi yazın.
+
+
+function isValidVariable() {
+
+    
+};
+
+//TODO
