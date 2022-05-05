88e31a2e1daf0672a41f837c4112e3e50abc7c30
Nurcan Cürebal <nurcancurebal@hotmail.com>
2022-05-05 19:04:23 +0300

    .

--- objects/scope-one.js
+++ objects/scope-one.js
@@ -1,3 +1,9 @@
 // değişkenlerin kapsamları(scopeları) şunlar olabilir
 // 1. Global
 // 2. Local
+// let, const ya da var ile değişken tanımlanmamışsa " Window Global Object " olarak geçer.
+
+// Window Global Object = değişkeni let, const, var ile tanımlamadanda görebiliriz. bu değişkenler windows global object olur
+
+name = 'nurcan';
+console.log(name);
