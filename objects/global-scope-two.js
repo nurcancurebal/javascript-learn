8d4b643e5337f128475e95e41d71b8b6f495db5d
Nurcan Cürebal <nurcancurebal@hotmail.com>
2022-05-09 13:58:52 +0300

    .

---
+++ objects/global-scope-two.js
@@ -0,0 +1,22 @@
+// global olarak tanımlanmış değişken aynı dosyada her yerden erişelebilir. global dediğimiz bütün dosya içindede global
+// olabilir yada bir kod bloğu üzerindede globalolarak tanımlanmış olabilir
+
+let a = 'JavaScript'; // bu dosyanın her hangi bir yerinden ulaşılabilir.
+let b = 10;
+
+function letsLearnScope() {
+
+    console.log(a, b); // JavaScript 10
+
+    if (true) {         //! burada bir blok var. blok= süslü parantezlerin içerisinde kalan kısım
+
+        let a = 'Python';
+        let b = 100;
+
+        console.log(a, b); //! Python 100 (yalnızca bloğun içinde erişebiliyoruz)
+    };
+    console.log(a, b);   // JavaScript 10
+};
+
+letsLearnScope();
+console.log(a, b); // JavaScript 10
