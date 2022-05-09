4f1e63e4a96582ea21db5a9d02df83e63e359054
Nurcan Cürebal <nurcancurebal@hotmail.com>
2022-05-09 19:39:00 +0300

    .

--- arrays/arreyden-eleman-cikarip-ekleme-splice.js
+++ arrays/arreyden-eleman-cikarip-ekleme-splice.js
@@ -1,7 +1,7 @@
 const numbers = [1, 2, 3, 4, 5, 6];
 
 let newNumbers = numbers.splice(1, 4, 'tuğçe', 'nurcan');  //(4) [2, 3, 4, 5] = çıkartılan elemanları buraya yazar
-// başlangıç indeksi, kaç tane eleman silinecek, eklenen elemanlar (3 tane değer giriyoruz)
+// (silmeye başladığı indeks, kaç tane eleman silinecek, eklenen elemanlar)
 
 console.log(numbers); //(4) [1, 'tuğçe', 'nurcan', 6] = son oluşan şeklide sona yazar
 
--- objects/yeni-bir-key-anahtar-tanimlama-eight.js
+++ objects/yeni-bir-key-anahtar-tanimlama-eight.js
@@ -1,11 +1,11 @@
 // objemiz değiştirilebilir(mutable) bir kez tanımlandıktan sonra onu değiştirebiliriz
 
 const person = {
-    firstName: 'Asabeneh',
-    lastName: 'Yetayeh',
-    age: 250,
-    country: 'Finland',
-    city: 'Helsinki',
+    firstName: 'Nurcan',
+    lastName: 'Cürebal',
+    age: 27,
+    country: 'Canada',
+    city: 'Ottava',
     skills: [
         'HTML',
         'CSS',
@@ -21,16 +21,18 @@ const person = {
 };
 
 person.nationality = 'Ethiopian';
-person.country = 'Finland';
-person.title = 'teacher';
-person.skills.push('Meteor');
-person.skills.push('SasS');
-person.isMarried = true;
+person.country = 'Finland'; // countryi değiştirdik yeni değeri
+person.title = 'teacher'; // person objesine eklendi
+person.skills.push('Meteor'); // skills array olduğu için sondan ekledik push ile
+person.skills.push('SasS')
+person.isMarried = true; // person objesine eklendi
+//! person objesine eklenenler sondan eklenmez, person objesinin içi a dan z ye kadar sıralanır, eklenenlerde bu sıraya göre eklenir.
+
 
 person.getPersonInfo = function () {
 
     let skillsWithoutLastSkill = this.skills
-        .splice(0, this.skills.length - 1)
+        .splice(0, this.skills.length - 1)  //TODO burada skillsin içini boşaltıyor nasıl oluyor anlamadım
         .join(', ');
     let lastSkill = this.skills.splice(this.skills.length - 1)[0];
 
@@ -40,5 +42,9 @@ person.getPersonInfo = function () {
     return statement;
 };
 
-console.log(person);
+console.log(person); // {firstName: 'Nurcan', lastName: 'Cürebal', age: 27, country: 'Finland', city: 'Ottava', …}
+
 console.log(person.getPersonInfo());
+// Nurcan Cürebal is a teacher.
+// He lives in Finland.
+// He teaches HTML, CSS, JavaScript, React, Node, MongoDB, Python, D3.js, Meteor, and sasS.
