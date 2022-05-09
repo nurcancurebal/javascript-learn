99a955859880788c9d9892b405e35120b04a7e30
Nurcan Cürebal <nurcancurebal@hotmail.com>
2022-05-09 13:33:47 +0300

    .

--- objects/window-global-object-one.js
+++ objects/window-global-object-one.js
@@ -3,8 +3,9 @@
 // 2. Local
 // let, const ya da var ile değişken tanımlanmamışsa " Window Global Object " olarak geçer.
 
-// Window Global Object = değişkeni let, const, var ile tanımlamadanda görebiliriz. bu değişkenler windows global object olur
+//! WİNDOW GLOBAL OBJECT = değişkeni let, const, var ile tanımlamadanda görebiliriz. bu değişkenler windows global object olur
 
 name = 'nurcan';
 console.log(name);
 
+window.name
