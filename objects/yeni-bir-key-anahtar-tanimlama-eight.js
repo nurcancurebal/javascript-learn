2eb13556e1566db43e5c02837fae22c6b1e960af
Nurcan Cürebal <nurcancurebal@hotmail.com>
2022-05-09 19:47:44 +0300

    .

--- objects/yeni-bir-key-anahtar-tanimlama-eight.js
+++ objects/yeni-bir-key-anahtar-tanimlama-eight.js
@@ -29,15 +29,17 @@ person.isMarried = true; // person objesine eklendi
 //! person objesine eklenenler sondan eklenmez, person objesinin içi a dan z ye kadar sıralanır, eklenenlerde bu sıraya göre eklenir.
 
 
+// function ekleme
+
 person.getPersonInfo = function () {
 
     let skillsWithoutLastSkill = this.skills
         .splice(0, this.skills.length - 1)  //TODO burada skillsin içini boşaltıyor nasıl oluyor anlamadım
         .join(', ');
     let lastSkill = this.skills.splice(this.skills.length - 1)[0];
-
     let skills = `${skillsWithoutLastSkill}, and ${lastSkill}`;
-    let fullName = this.getFullName();
+
+    let fullName = this.getFullName(); // objenin içindeki fullname functionunu çağırmış
     let statement = `${fullName} is a ${this.title}.\nHe lives in ${this.country}.\nHe teaches ${skills}.`;
     return statement;
 };
