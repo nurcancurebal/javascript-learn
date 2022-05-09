7485b42ff1a21c527f67ded356c15b0afd069e98
Nurcan Cürebal <nurcancurebal@hotmail.com>
2022-05-09 20:30:11 +0300

    .

--- objects/yeni-bir-key-anahtar-tanimlama-eight.js
+++ objects/yeni-bir-key-anahtar-tanimlama-eight.js
@@ -33,10 +33,11 @@ person.isMarried = true; // person objesine eklendi
 
 person.getPersonInfo = function () {
 
-    let skillsWithoutLastSkill = this.skills
-        .splice(0, this.skills.length - 1)  //TODO burada skillsin içini boşaltıyor nasıl oluyor anlamadım
-        .join(', ');
-    let lastSkill = this.skills.splice(this.skills.length - 1)[0];
+    let skillsWithoutLastSkill = this.skills.splice(0, this.skills.length - 1).join(', ');
+    // HTML, CSS, JavaScript, React, Node, MongoDB, Python, D3.js, Meteor (skillsin içinden 0' ıncı değerden başladık 9 tane değeri
+    // aldık ve birleştirdik onuda skillsWithoutLastSkill' in içine attık)
+
+    let lastSkill = this.skills.splice(this.skills.length - 1)[0]; // TODO burada ne yaptı
     let skills = `${skillsWithoutLastSkill}, and ${lastSkill}`;
 
     let fullName = this.getFullName(); // objenin içindeki fullname functionunu çağırmış
