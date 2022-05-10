5b241dade3ce9251bab06e77d63ea694b1130bc8
Nurcan Cürebal <nurcancurebal@hotmail.com>
2022-05-10 22:03:07 +0300

    .

---
+++ objects/objenin-anahtarlarini-dizi-olarak-almak-objectkeys-eleven.js
@@ -0,0 +1,26 @@
+// Object.keys: Bir nesnenin anahtarlarını veya özelliklerini array olarak almak için
+
+const person = {
+    firstName: 'Asabeneh',
+    age: 250,
+    country: 'Finland',
+    city:'Helsinki',
+    skills: ['HTML', 'CSS', 'JS'],
+    title: 'teacher',
+    address: {
+      street: 'Heitamienkatu 16',
+      pobox: 2002,
+      city: 'Helsinki'
+    },
+    getPersonInfo: function() {
+      return `I am ${this.firstName} and I live in ${this.city}, ${this.country}. I am ${this.age}.`
+    }
+  };
+  
+  const keys = Object.keys(person);
+
+  console.log(keys); // (8) ['firstName', 'age', 'country', 'city', 'skills', 'title', 'address', 'getPersonInfo']
+
+  const newAddress = Object.keys(person.address);
+
+  console.log(newAddress); // (3) ['street', 'pobox', 'city']
