02711afb25da18af77a1bfa690465424037ca005
Nurcan Cürebal <nurcancurebal@hotmail.com>
2022-05-10 21:53:06 +0300

    .

---
+++ objects/objeyi-referanssız-kopyalama-objectassign-ten.js
@@ -0,0 +1,29 @@
+// Object.assign: Orijinal nesneyi değiştirmeden bir nesneyi kopyalamak için, referanssız kopyalama
+
+const person = {
+    firstName: 'Nurcan',
+    age: 250,
+    country: 'Finland',
+    getPersonInfo: function () {
+        return `I am ${this.firstName} and I live in ${this.city}, ${this.country}. I am ${this.age}.`;
+    }
+};
+
+// referanslı kopyalama
+
+let newPerson= person;
+console.log(person); // {firstName: 'Nurcan', age: 250, country: 'Finland', getPersonInfo: ƒ}
+
+newPerson.firstName = 'Erdinç';
+console.log(person); // {firstName: 'Erdinç', age: 250, country: 'Finland', getPersonInfo: ƒ}
+
+//! referanslı kopyalarsak orijinal metinde her değişimden etkilenir.
+
+// referanssız kopyalama
+
+const copyPerson = Object.assign({}, person);
+
+copyPerson.firstName ='Bal';
+
+console.log(person);
+console.log(copyPerson);
