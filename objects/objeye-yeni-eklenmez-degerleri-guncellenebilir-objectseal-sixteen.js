c718d6155a6b26034056c02f9315e36aa0dcde7b
Nurcan Cürebal <nurcancurebal@hotmail.com>
2022-05-12 13:32:36 +0300

    .

---
+++ objects/objeye-yeni-eklenmez-degerleri-guncellenebilir-objectseal-sixteen.js
@@ -0,0 +1,21 @@
+// Object.seal() yöntemi, bir nesneyi mühürler, ona yeni özelliklerin eklenmesini önler ve mevcut tüm özellikleri yapılandırılamaz
+// olarak işaretler. Mevcut özelliklerin değerleri, yazılabilir oldukları sürece değiştirilebilir.
+
+const person = {
+    firstName: 'Nurcan',
+    age: 27,
+    country: 'Canada',
+    city: 'Ottava',
+    skills: ['HTML', 'CSS', 'JS']
+};
+
+Object.seal(person);
+
+person.nationality = 'Ethiopian'; // eklemedi
+person.country = 'Ireland'; // değiştirdi
+person.title = 'teacher'; // eklemedi
+person.skills.push('Meteor'); // var olana ekledi
+person.skills.push('SasS'); // var olana ekledi
+person.isMarried = true; // eklemedi
+
+console.log(person); // {firstName: 'Nurcan', age: 27, country: 'Ireland', city: 'Ottava', skills: Array(5)}
