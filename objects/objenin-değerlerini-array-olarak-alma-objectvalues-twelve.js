afbec60318b9e4ff16b9fcc7e030b40f92c5b32b
Nurcan Cürebal <nurcancurebal@hotmail.com>
2022-05-12 12:07:08 +0300

    .

--- objects/objenin-değerlerini-array-olarak-alma-objectvalues-twelve.js
+++ objects/objenin-değerlerini-array-olarak-alma-objectvalues-twelve.js
@@ -1,2 +1,22 @@
 // Object.values: Bir nesnenin değerlerini dizi olarak almak için
 
+const person = {
+    firstName: 'Nurcan',
+    age: 27,
+    country: 'Canada',
+    city:'Ottava',
+    skills: ['HTML', 'CSS', 'JS'],
+    title: 'teacher',
+    address: {
+      street: 'Heitamienkatu 16',
+      pobox: 2002,
+      city: 'Helsinki'
+    },
+    getPersonInfo: function() {
+      return `I am ${this.firstName} and I live in ${this.city}, ${this.country}. I am ${this.age}.`
+    }
+  };
+
+  const values = Object.values(person);
+
+  console.log(values);  //(8) ['Nurcan', 27, 'Canada', 'Ottava', Array(3), 'teacher', {…}, ƒ]
