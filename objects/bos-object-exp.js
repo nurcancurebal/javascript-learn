7ccb82a0427ce7ff25cf24c9f1b6dbdab92b1d0e
Nurcan Cürebal <nurcancurebal@hotmail.com>
2022-05-12 16:19:00 +0300

    .

---
+++ objects/bos-object-exp.js
@@ -0,0 +1,16 @@
+const dog = {
+    name: 'kont',
+    legs: 4,
+    color: 'black',
+    age: 7,
+    bark: 'woof woof'
+};
+
+console.log(dog);
+
+dog.breed = 'Bulldog';
+dog.getDogInfo = 'Barren';
+
+console.log(dog);
+
+//TODO level 1 soru 4 ne istiyor?
