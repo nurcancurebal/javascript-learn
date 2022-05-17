267287aeb2cfed2188efaf9a520cdb3b5ea8662b
Nurcan Cürebal <nurcancurebal@hotmail.com>
2022-05-17 16:51:22 +0300

    .

--- arrays/birlestirip-stringe-cevir-join.js
+++ arrays/birlestirip-stringe-cevir-join.js
@@ -3,6 +3,11 @@
 
 const names = ['Asabeneh', 'Mathias', 'Elias', 'Brook'];
 
+let newNames = names.join();
+
+console.log(typeof names, typeof newNames); // object string
+
+
 console.log(names.join()); // Asabeneh,Mathias,Elias,Brook
 
 console.log(names.join('')); //AsabenehMathiasEliasBrook
@@ -16,4 +21,7 @@ console.log(names.join(' # ')); //Asabeneh # Mathias # Elias # Brook
 const number = [151, 616, 397542, 738, 62];
 
 console.log(number.join()); //151,616,397542,738,62
-console.log(typeof number); // veri tipi object
+
+let newNumber = number.join();
+
+console.log(typeof number, typeof newNumber); // object string
--- functions/function-learn-one.js
+++ functions/function-learn-one.js
@@ -1,10 +1,13 @@
 const birinciSayi = 46;
 const ikinciSayi = 50;
-const toplaBir = birinciSayi + ikinciSayi;
 
-const toplaIki = sayilariTopla(birinciSayi, ikinciSayi);
+const toplaBir = birinciSayi + ikinciSayi; // bu şekildede iki sayıyı toplatabiliriz
+
+
+const toplaIki = sayilariTopla(birinciSayi, ikinciSayi); // fonksiyonu kullanarak sayıları topladı
+
+
 const toplaUc = sayilariTopla(10, 15);
-
 
 function sayilariTopla(birinciDeger, ikinciDeger) {
 
@@ -13,3 +16,5 @@ function sayilariTopla(birinciDeger, ikinciDeger) {
 };
 /* toplama işlemini bir değişkene atadık her seferinde bu değişkeni kullanarak farklı sayılar yazarak istediğimiz
 kadar sayıyı kısa yoldan her seferinde toplama işlemini baştan yaptırmayarak istediğimiz kadar sayı toplatabiliriz  */
+
+console.log(toplaBir, toplaIki , toplaUc);
