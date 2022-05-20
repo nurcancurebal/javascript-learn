97f158b432d884520155592dde98318825669693
Nurcan Cürebal <nurcancurebal@hotmail.com>
2022-05-20 22:11:41 +0300

    .

---
+++ objects/exp.js
@@ -0,0 +1,32 @@
+// Ürünler arrayinin üç elemanı vardır ve her birinin altı özelliği vardır. a. Ürünü derecelendiren RateProduct adında bir fonksiyon
+// oluşturun b. Bir ürünün ortalama derecelendirmesini hesaplayan ortalamaRating adlı bir fonksiyon oluşturun.
+
+const products = [
+    {
+        _id: 'eedfcf',
+        name: 'mobile phone',
+        description: 'Huawei Honor',
+        price: 200,
+        ratings: [
+            { userId: 'fg12cy', rate: 5 },
+            { userId: 'zwf8md', rate: 4.5 }],
+        likes: []
+    },
+    {
+        _id: 'aegfal',
+        name: 'Laptop',
+        description: 'MacPro: System Darwin',
+        price: 2500,
+        ratings: [],
+        likes: ['fg12cy']
+    },
+    {
+        _id: 'hedfcg',
+        name: 'TV',
+        description: 'Smart TV:Procaster',
+        price: 400,
+        ratings: [{ userId: 'fg12cy', rate: 5 }],
+        likes: ['fg12cy']
+    }
+];
+
