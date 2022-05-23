cc23277bef17d983272dfb9db6fbabfa96e9cde0
Nurcan Cürebal <nurcancurebal@hotmail.com>
2022-05-23 20:26:43 +0300

    .

---
+++ higher-order fonksiyonlar/foreach-three.js
@@ -0,0 +1,21 @@
+// 1. kullanımı
+
+const numbers = [1, 3, 5, 7, 9];
+
+let total = 0;
+
+numbers.forEach(number => total += number); //! forEach()parametre olarak bir fonksiyon alır.
+
+console.log(total);
+
+// 2. kullanımı
+
+const num = [10, 11, 12, 13];
+
+let totalTwo = 0;
+
+let calculate = sayi => totalTwo += sayi;
+
+num.forEach(calculate);
+
+console.log(totalTwo);
---
+++ higher-order fonksiyonlar/returning-function-two.js
@@ -0,0 +1,16 @@
+// Daha yüksek dereceli fonksiyonlar, fonksiyonu bir değer olarak döndürür​
+
+const a = s1 => {
+
+    const b = s2 => {
+
+        const c = s3 => {
+
+            return s1 + s2 + s3; // c fonksiyonunun çıktısı
+        };
+        return c; // b fonksiyonunun çıktısı
+    };
+    return b; // a fonksiyonunun çıktısı
+};
+
+console.log(a(5)(10)(20));
