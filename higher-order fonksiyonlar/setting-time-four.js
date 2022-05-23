8dad0948ba95567024eff8778cf05c50b0b26fc4
Nurcan Cürebal <nurcancurebal@hotmail.com>
2022-05-23 22:16:37 +0300

    .

---
+++ higher-order fonksiyonlar/setting-time-four.js
@@ -0,0 +1,9 @@
+// JavaScript'te bazı aktiviteleri belirli bir zaman aralığında yürütebiliriz veya bazı aktiviteleri yürütmek
+// için bir süre planlayabiliriz(bekleyebiliriz).
+
+// setInterval= belli aralıklarla çalışacak olan fonksiyonumuz
+// setTimeout= tek seferlik, ne kadar zaman sonra çalışacağını ayarlayabiliyoruz
+
+//! 1000 milisaniye 1 saniyeye eşit
+
+
