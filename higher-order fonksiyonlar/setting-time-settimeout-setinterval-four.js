d1057a127e0da42142f64a52009422e54c405523
Nurcan Cürebal <nurcancurebal@hotmail.com>
2022-05-23 22:55:14 +0300

    .

--- higher-order fonksiyonlar/setting-time-settimeout-setinterval-four.js
+++ higher-order fonksiyonlar/setting-time-settimeout-setinterval-four.js
@@ -8,13 +8,27 @@
 
 
 function sayHello() {
-    
+
     console.log('Hello!');
 };
 
-setTimeout(sayHello,5000); // 5 saniye sonra çalıştı
-setInterval(sayHello,2000); // 2 saniyede bir çalışır
+// setTimeout(sayHello,2000); // 2 saniye sonra çalıştı
+// setInterval(sayHello,5000); // 5 saniyede bir çalışır
 
 //! clearInterval ile setInterval' i iptal edebiliriz
 //! clearTimeout ile setTimeout' u iptal edebiliriz
 
+const interval = setInterval(sayHello, 1000);
+
+const timeOut = setTimeout(() => {
+
+    clearInterval(interval);
+    console.log('interval işlemi iptal edildi');
+
+}, 5000); // 5 saniye sonra interval işlemini iptal edecek
+
+setTimeout(() => {
+
+    clearTimeout(timeOut);
+    console.log('time out işlemi iptal edildi, interval devam edecek');
+}, 3000); 
