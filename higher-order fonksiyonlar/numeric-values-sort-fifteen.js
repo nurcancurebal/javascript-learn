646d70b951b56d5304405fdbb02fb0a8d24ee17b
Nurcan Cürebal <nurcancurebal@hotmail.com>
2022-05-25 23:50:00 +0300

    .

--- higher-order fonksiyonlar/numeric-values-sort-fifteen.js
+++ higher-order fonksiyonlar/numeric-values-sort-fifteen.js
@@ -1,2 +1,19 @@
-// numaraları sıralamada kullanılır. Parametre olarak fonksiyon alır.
+// numaraları sıralamada kullanılır. Parametre olarak fonksiyon alır. a ve b iki parametresidir, eğer a-b yaparsanız
+//  küçükten büyüğe, b-a yaparsanız büyükten küçüğe sıralar.
 
+const numbers = [9.81, 3.14, 100, 37];
+
+const numberSort = numbers.sort(function (a, b) {
+
+    return a - b;
+});
+
+console.log(numberSort);
+
+
+const numberSortTwo = numbers.sort(function (a, b) {
+
+    return b - a;
+});
+
+console.log(numberSortTwo);
