ffac35714d8a69ff4fe972a0a0f9f6a19176936f
Nurcan Cürebal <nurcancurebal@hotmail.com>
2022-05-25 18:13:35 +0300

    .

---
+++ higher-order fonksiyonlar/reduce-nine.js
@@ -0,0 +1,12 @@
+// Reduce: Azalt demektir. Bir callback  functionu alır. Callback fonksiyonu parmetre olarak toplayıcı (accumulator),
+// mevcut olanı (current) ve opsiyon olarakda varsayılan değeri alır. Geriye tek bir değer döndürür. Accumulator' e bir
+// başlangıç değeri vermek iyi bir uygulamadır. Eğer varsayılan olarak bir başlangıç değeri vermezsek o zaman dizinin ilk
+//  elemanını alır. Eğer dizimizde boş bir arrayse o zaman javascript hata verir.
+
+const numbers = [1, 2, 3, 4, 5, 6, 7];
+
+console.log(numbers.reduce(function (oldValue, CurrentValue) {
+
+    return oldValue + CurrentValue; // burada 0' ı oldValue' ya verdik
+
+}, 0));
