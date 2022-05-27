2bae6f93eff3bad349faf93c9a91f19e31424ade
Nurcan Cürebal <nurcancurebal@hotmail.com>
2022-05-27 22:40:19 +0300

    .

--- higher-order fonksiyonlar/foreach-map-filter-reduce-exp.js
+++ higher-order fonksiyonlar/foreach-map-filter-reduce-exp.js
@@ -1,13 +1,25 @@
 const products = [
-    { product: 'banana', price: 3 },
-    { product: 'mango', price: 6 },
+    { product: 'banana', price: 3 }, // entries: 0: (2) ['product', 'banana']
+    { product: 'mango', price: 6 },           // 1: (2) ['price',3] şelinde arreye çevirir.
     { product: 'potato', price: ' ' },
     { product: 'avocado', price: 8 },
     { product: 'coffee', price: 10 },
     { product: 'tea', price: '' },
 ];
 
-console.log(typeof products); // object
+const newProducts = []
 
+products.forEach(function (productUpper) {
+
+    const convertObjectToArray = Object.entries(productUpper);
+
+    convertObjectToArray[0][1] = String(convertObjectToArray[0][1]).toUpperCase();
+
+    const oldConvert = Object.fromEntries(convertObjectToArray); // Entries üzerinden nesne klonu oluşturmak için kullanılır.
+
+    newProducts.push(oldConvert);
+});
+
+console.log(newProducts);
 
 
