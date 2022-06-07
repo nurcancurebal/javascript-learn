5644be76c9be35b7561c84ec1d4487978f207e67
Nurcan Cürebal <nurcancurebal@hotmail.com>
2022-06-07 17:56:19 +0300

    .

--- higher-order fonksiyonlar/foreach-simple-exp.js
+++ higher-order fonksiyonlar/foreach-simple-exp.js
@@ -1,6 +1,30 @@
+//! forEach( return dönmez)
+
 const countries = ['Finland', 'Sweden', 'Denmark', 'Norway', 'IceLand'];
+
+const names = ['Nurcan', 'Erdinç', 'Bal', 'Canada'];
+
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+
 
 countries.forEach(function (country) {
 
     console.log(country);
 });
+
+
+
+const namesForeach = names.forEach(function (name) {
+
+    console.log(name);
+});
+
+
+
+numbers.forEach(num);
+
+function num(number) {
+
+    console.log(number);
+};
