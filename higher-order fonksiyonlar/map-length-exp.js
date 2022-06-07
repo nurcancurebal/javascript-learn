84b42836e0544d46ddfba36e6fec01c702467ecf
Nurcan Cürebal <nurcancurebal@hotmail.com>
2022-06-07 23:48:44 +0300

    .

---
+++ higher-order fonksiyonlar/map-length-exp.js
@@ -0,0 +1,9 @@
+const countries = ['Finland', 'Sweden', 'Denmark', 'Norway', 'IceLand'];
+
+
+const countriesLength = countries.map(function (country) {
+
+    return country.length;
+});
+
+console.log(countriesLength);
