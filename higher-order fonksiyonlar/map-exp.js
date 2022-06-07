ee3dfbdfa47bde7fe75b7553c52ff355da850c57
Nurcan Cürebal <nurcancurebal@hotmail.com>
2022-06-07 21:19:36 +0300

    .

---
+++ higher-order fonksiyonlar/map-exp.js
@@ -0,0 +1,31 @@
+const countries = ['Finland', 'Sweden', 'Denmark', 'Norway', 'IceLand'];
+
+const numbers = [65, 44, 12, 4];
+
+
+const bigCountries = countries.map(function (countriesToUpperCase) {
+
+    return countriesToUpperCase.toUpperCase();
+});
+
+console.log(bigCountries);  // [ 'FINLAND', 'SWEDEN', 'DENMARK', 'NORWAY', 'ICELAND' ]
+
+
+
+const countriesLength = countries.map(function (country) {
+
+    return country.length;
+});
+
+console.log(countriesLength);
+
+
+
+const newArr = numbers.map(myFunction);
+
+function myFunction(num) {
+
+    return num * 10;
+};
+
+console.log(newArr);    // [ 650, 440, 120, 40 ]
