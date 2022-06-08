163d964868c9aaab0d3d6990d6c53e3177613bd8
Nurcan Cürebal <nurcancurebal@hotmail.com>
2022-06-08 15:57:01 +0300

    .

--- higher-order fonksiyonlar/reduce-exp.js
+++ higher-order fonksiyonlar/reduce-exp.js
@@ -1,3 +1,5 @@
+// Declare a function called getStringLists which takes an array as a parameter and then returns an array only with string items.
+
 const products = [
     { product: 'banana', price: 3 },
     { product: 'mango', price: 6 },
@@ -9,5 +11,14 @@ const products = [
 
 const itemProduct = products.forEach(function (item) {
 
-    console.log(Object.values(item)[0]);
+    console.log(Object.values(item)[0].split(' '));
 });
+
+// [ 'banana' ]
+// [ 'mango' ]
+// [ 'potato' ]
+// [ 'avocado' ]
+// [ 'coffee' ]
+// [ 'tea' ]
+
+// TODO
