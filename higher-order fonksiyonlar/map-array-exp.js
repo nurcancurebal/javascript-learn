62dd8842e7d1d406b1c727fd6d0ef546fc1e578c
Nurcan Cürebal <nurcancurebal@hotmail.com>
2022-06-08 10:37:43 +0300

    .

---
+++ higher-order fonksiyonlar/map-array-exp.js
@@ -0,0 +1,14 @@
+const products = [
+    { product: 'banana', price: 3 },
+    { product: 'mango', price: 6 },
+    { product: 'potato', price: ' ' },
+    { product: 'avocado', price: 8 },
+    { product: 'coffee', price: 10 },
+    { product: 'tea', price: '' },
+  ];
+
+  const productPrice = products.map(function (corresponding) {
+      return corresponding;
+  });
+
+  console.log(productPrice);
---
+++ higher-order fonksiyonlar/map-touppercase-exp.js
@@ -0,0 +1,8 @@
+const names = ['Nurcan', 'Erdinç', 'Cürebal', 'Bal'];
+
+const nameToUpperCase = names.map(function (bigNames) {
+
+    return bigNames.toUpperCase();
+});
+
+console.log(nameToUpperCase);
