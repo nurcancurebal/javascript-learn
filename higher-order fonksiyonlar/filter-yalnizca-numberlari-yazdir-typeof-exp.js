8c123e0ba3fb9c6bfdbd800b0fb7f9ea547f3cd8
Nurcan Cürebal <nurcancurebal@hotmail.com>
2022-06-08 14:08:11 +0300

    .

---
+++ higher-order fonksiyonlar/filter-yalnizca-numberlari-yazdir-typeof-exp.js
@@ -0,0 +1,15 @@
+const products = [
+    { product: 'banana', price: 3 },
+    { product: 'mango', price: 6 },
+    { product: 'potato', price: ' ' },
+    { product: 'avocado', price: 8 },
+    { product: 'coffee', price: 10 },
+    { product: 'tea', price: '' },
+];
+
+let numberProducts = products.filter(function (num) {
+
+    return typeof num.price == number;
+});
+
+console.log(numberProducts);
