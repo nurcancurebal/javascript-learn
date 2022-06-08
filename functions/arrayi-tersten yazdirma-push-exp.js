7c77cf64a5d346565a836affc0b70b87a36f1581
Nurcan Cürebal <nurcancurebal@hotmail.com>
2022-06-09 00:27:44 +0300

    .

--- functions/arrayi-tersten yazdirma-push-exp.js
+++ functions/arrayi-tersten yazdirma-push-exp.js
@@ -12,9 +12,7 @@ function reverseArray(funcArray) {
     for (let index = (funcArray.length - 1); 0 <= index; index--) {
 
         items.push(funcArray[index]);
-
     };
 
     return items;
-
 };
