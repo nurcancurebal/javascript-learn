af3725cb154c3e785d8907ac7ed2de1877daad89
Nurcan Cürebal <nurcancurebal@hotmail.com>
2022-06-10 23:02:47 +0300

    .

---
+++ higher-order fonksiyonlar/returning-function-chained-filter-includes-foreach-exp.js
@@ -0,0 +1,28 @@
+// filtre kullanarak bir search atacak aradığın şeyde 4 taraftada arayacak uygun olan gelecek
+
+const countries = [
+    { nameEng: "Germany", nameTr: "Almanya", capital: "Berlin", stars: 17 },
+    { nameEng: "Denmark", nameTr: "Danimarka", capital: "Kopenhag", stars: 17 },
+    { nameEng: "Swiss", nameTr: "isviçre", capital: "Bern", stars: 17 },
+    { nameEng: "Finland", nameTr: "Finlandiya", capital: "Helsinki", stars: 16 },
+    { nameEng: "Norway", nameTr: "Norveç", capital: "Oslo", stars: 16 },
+    { nameEng: "Australia", nameTr: "Avustralya", capital: "Kanberra", stars: 15 },
+    { nameEng: "Sweden", nameTr: "İsveç", capital: "Stokholm", stars: 14 },
+    { nameEng: "Holland", nameTr: "Hollanda", capital: "Amsterdam", stars: 14 },
+    { nameEng: "Austria", nameTr: "Avusturya", capital: "Viyana", stars: 14 }
+];
+
+searchArray(17).forEach(({ nameEng, nameTr, capital, stars }) => {
+
+    console.log(`Ülkenin ismi İngilizce/Türkçe : ${nameEng}/${nameTr} `);
+});
+
+function searchArray(value) {
+
+    const result = countries.filter(function (item) {
+
+        return item.nameEng.includes(value) || item.nameTr.includes(value) || item.capital.includes(value) || item.stars == value;
+    });
+
+    return result;
+};
