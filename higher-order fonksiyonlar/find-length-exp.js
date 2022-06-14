15c26cee04ffa1cac03bfd73f8135d8cf2151140
Nurcan Cürebal <nurcancurebal@hotmail.com>
2022-06-14 19:32:43 +0300

    .

--- higher-order fonksiyonlar/find-length-exp.js
+++ higher-order fonksiyonlar/find-length-exp.js
@@ -5,4 +5,4 @@ let countriesFind = countries.find(function (item) {
     return item.length == 6;
 });
 
-console.log(countriesFind); // Sweden
+console.log(countriesFind); // Sweden : uzunluğu 6 olan ilk ülkeyi yazdırdı
