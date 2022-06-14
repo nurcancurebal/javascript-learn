e8a186a8bcda122bc92e2ce60f1bdc51df549587
Nurcan Cürebal <nurcancurebal@hotmail.com>
2022-06-14 11:33:45 +0300

    .

---
+++ higher-order fonksiyonlar/afg.js
@@ -0,0 +1,7 @@
+const countries = ['Germany', 'Denmark', 'Swiss', 'Finland', 'Norway', 'Australia', 'Sweden', 'Holland', 'Austria', 'Canada', 'IceLand'];
+
+
+function getLastTenCountries(params) {
+    
+}
+
---
+++ higher-order fonksiyonlar/dfgh.js
@@ -0,0 +1,10 @@
+// Declare a getFirstTenCountries function and return an array of ten countries. Use different functional programming to work on the countries.js array
+
+const countries = ['Germany', 'Denmark', 'Swiss', 'Finland', 'Norway', 'Australia', 'Sweden', 'Holland', 'Austria', 'Canada'];
+
+
+function getFirstTenCountries() {
+    
+} 
+
+// TODO
