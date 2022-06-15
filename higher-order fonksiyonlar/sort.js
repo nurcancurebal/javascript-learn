811ad248a06fba5d0be64fb0cd10505cf698c451
Nurcan Cürebal <nurcancurebal@hotmail.com>
2022-06-15 22:44:34 +0300

    .

---
+++ higher-order fonksiyonlar/sort.js
@@ -0,0 +1,11 @@
+const countries = {
+    country: 'Germany', capital: 'Berlin', population: 83.24,
+    country: 'Denmark', capital: 'Kopenhag', population: 5.831,
+    country: 'Swiss', capital: 'Bern', population: 8.637,
+    country: 'Finland', capital: 'Helsinki', population: 5.531,
+    country: 'Norway', capital: 'Oslo', population: 5.379,
+    country: 'Australia', capital: 'Kanberra', population: 25.69,
+    country: 'Sweden', capital: 'Stokholm', population: 10.35,
+    country: 'Holland', capital: 'Amsterdam', population: 17.44,
+    country: 'Austria', capital: 'Viyana', population: 8.917
+};
