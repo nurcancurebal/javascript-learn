44c87f5ca8ed8fab297e99d689e46f56d9488b97
Nurcan Cürebal <nurcancurebal@hotmail.com>
2022-06-15 13:34:15 +0300

    .

--- higher-order fonksiyonlar/callback-obje-olusturma-difficult-ekrana-yazdirma-exp.js
+++ higher-order fonksiyonlar/callback-obje-olusturma-difficult-ekrana-yazdirma-exp.js
@@ -13,17 +13,17 @@ function main(pusher, listen) {
     pusher("Nurcan", "Ben sekerlikten oluşan bir hanımefendiyim ya siz?");
     pusher("Erdinç", "Sanane");
 
-    listen();
+    listen(); // listen function olduğu için bu şekilde burada çalıştırdık
 };
 
 function pusherFunc(who, message) {
 
-    messages.push({ who, message });
+    messages.push({ who, message }); // obje olarak messages' in içine pushladı
 };
 
 function listenerFunc() {
 
     messages.forEach(function (item) {
-        console.log(`WHO: ${item.who} | MESSAGE: ${item.message}`);
+        console.log(`WHO: ${item.who} | MESSAGE: ${item.message}`); // item: {who: 'NODE.JS', message: 'Go go go!'} item bu şekildeyken bunu tek tek düzenleyip ekrana yazdırıyor
     });
 };
--- higher-order fonksiyonlar/sdfgh.js
+++ higher-order fonksiyonlar/sdfgh.js
@@ -2,4 +2,10 @@
 
 const countries = ['Germany', 'Denmark', 'Swiss', 'Finland', 'Norway', 'Australia', 'Sweden', 'Holland', 'Austria'];
 
+const countriesMap = countries.map(function (country) {
 
+    
+
+
+    
+})
