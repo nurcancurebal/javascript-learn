9c83430916999b5a0367a8720e1425a8c372b915
Nurcan Cürebal <nurcancurebal@hotmail.com>
2022-06-17 01:13:11 +0300

    .

--- higher-order fonksiyonlar/dfgh.js
+++ higher-order fonksiyonlar/dfgh.js
@@ -1,10 +1,52 @@
-// 20 ülke olacak, ortadan beş tane getir veya soldan 5 veya sağdan 5 tane getir, fonksiyon 2 parametre alır 1. parametre nereden getireceği 2. kaç adet geirecek forEach yada reduce
+// 20 ülke olacak, ortadan beş tane getir veya soldan 5 veya sağdan 5 tane getir, fonksiyon 2 parametre alır 1. parametre nereden getireceği 2. kaç adet getirecek forEach yada reduce
 
-const countries = ['Germany', 'Denmark', 'Swiss', 'Finland', 'Norway', 'Australia', 'Sweden', 'Holland', 'Austria', 'Canada'];
+const countries = [
+  "Germany",
+  "Denmark",
+  "Swiss",
+  "Finland",
+  "Norway",
+  "Australia",
+  "Sweden",
+  "Holland",
+  "Austria",
+  "Canada",
+  "China",
+  "India",
+  "United States of America",
+  "Indonesia",
+  "Brazil",
+  "Pakistan",
+  "Nigeria",
+  "Bangladesh",
+  "Japan",
+  "Russian Federation",
+];
 
 
-function getFirstTenCountries() {
-    
-} 
+console.log(getFirstTenCountries(countries, 5));
+
+
+function getFirstTenCountries(array, number) {
+
+  const result = countries.forEach(function (_country, index, arr) {
+
+    if (index < number) {
+
+      return array[index];
+
+    } else if (arr.lenght / 2 - 3 < index < arr.lenght / 2 + 3) {
+
+      return array[index];
+
+    } else if (arr.lenght - number <= index) {
+
+      return array[index];
+
+    }
+  });
+
+  return result;
+}
 
 // TODO
--- math/sayiyi-yuvarlama-math-ceil-floor-round.js
+++ math/sayiyi-yuvarlama-math-ceil-floor-round.js
@@ -1,15 +1,16 @@
 // Math.ceil()= sayıyı yukarı yuvarlar.5.6357 veya 5.9 olsun math.ceil sayıyı 6 yapar.
 
-
 var sayi = 7.5;
 
 console.log(Math.ceil(sayi)); //8
+
 
 //Math.floor() = Sayı ne olursa olsun aşağı yuvarlar.4.1 veya 4.9 çıktısı 4 dür.
 
 var sayi2 = 7.5;
 
 console.log(Math.floor(sayi2)); //7
+
 
 // Math.round()= sayı .5 ve üstü ise yukarı yuvarlar..5 altı ise aşağı yuvarlar.8.5 i 9 a 8.4 ü 8 yuvarlar.
 
