8b606a39c2a7f443d35abae63283e9c514f64b53
Nurcan Cürebal <nurcancurebal@hotmail.com>
2022-06-20 22:11:02 +0300

    .

--- higher-order-fonksiyonlar/returning-function-funtion-icinde-obje.js
+++ higher-order-fonksiyonlar/returning-function-funtion-icinde-obje.js
@@ -1,5 +1,3 @@
-// bir fonksiyon oluştur, içinde obje dönsün içindeki verileri yazdır.
-
 const ages = [31, 26, 34, 37, 27, 26, 32, 32, 26, 27, 27, 24, 32, 33, 27, 25, 26, 38, 37, 31, 34, 24, 33, 29, 26];
 
 
