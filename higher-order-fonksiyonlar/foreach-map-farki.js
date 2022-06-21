991fb0fa3d494b84d781d7319d4803b2116fbe08
Nurcan Cürebal <nurcancurebal@hotmail.com>
2022-06-21 18:51:32 +0300

    .

---
+++ higher-order-fonksiyonlar/foreach-map-farki.js
@@ -0,0 +1,21 @@
+const numbers = [1, 4, 9];
+
+
+numbers.forEach(function (number, index) {
+
+    numbers[index] = number * 2;
+});
+
+console.log(numbers);
+
+
+
+const numbersTwo = [1, 4, 9];
+
+
+const result = numbersTwo.map(function (number) {
+
+    return number * 2;
+});
+
+ console.log(result);
