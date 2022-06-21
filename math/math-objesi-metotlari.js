c1bbfd6cce5b88251a3d0ae1dd1b5ac8d4b3d550
Nurcan Cürebal <nurcancurebal@hotmail.com>
2022-06-21 17:57:36 +0300

    .

---
+++ math/math-objesi-metotlari.js
@@ -0,0 +1,57 @@
+/* matematik metotları 
+
+
+Math.random()	0-1 arasında rastgele sayı üretir.
+
+Math.min(a,b,c,d,…,z)	Kendisine parametre olarak gönderilen değerler arasında en küçük değeri getirir.
+
+Math.max(a,b,c,d,…,z)	Kendisine parametre olarak gönderilen değerler arasında en büyük değeri getirir.
+
+Math.round(x)	ondalıklı sayıyı en yakın tam sayıya yuvarlar.
+
+Math.ceil(x)	ondalıklı sayıyı kendisinden sonraki ilk tam sayıya yuvarlar.(yukarı yuvarlar)
+
+Math.floor(x)	ondalıklı sayıyının ondalıklı kısmını kaldırır.(aşağı yuvarlar)
+
+Math.sqrt(x)	Kendisine parametre olarak gönderilen sayının karesini verir.
+
+Math.pow(x,y)	x taban, y üs olacak şekilde sayının kuvvetini hesaplar
+
+Math.abs(x)	Sayının mutlak değerini verir. ( |-2| =2 )
+
+Math.acos(x)	-1 +1 arasındaki consinüs değerini radyan cinsinden döndürür.
+
+Math.asin(x)	-1 +1 arasındaki sinüs değerini 0 – pi arasında derecesini getirir.
+
+Math.atan(x)	tanjant tipindeki bir ifadenin 0 – pi arasında derecesini getirir.
+
+Math.atan2(y,x)	y,x değeri girilen ifadelerin 0 – pi arasında derecesini getirir.
+
+Math.cos(x)	derece cinsinden girilen cosinüs açısının -1 +1 arasıdaki değerini getirir.
+
+Math.exp(x)	ex  degerini getirir.
+
+Math.log(x)	e tabanındaki doğal logaritmasını verir.
+
+Math.sin(x)	derece cinsinden girilen sinüs açısının -1 +1 arasıdaki değerini getirir.
+
+Math.tan(x)	derece cinsinden girilen sayının radyan cinsinden değerini döndürür.
+
+
+Matematiksel Sabitler
+
+Math.E         	Bu özellik matematikteki euler sabitini verir.
+
+Math.PI      	Bu özellik matematikteki Pi sabitini verir.
+
+Math.SQRT2     	Bu özellik matematikteki 2 sayısının karekökünü verir.
+
+Math.SQRT1_2   	Bu özellik matematikteki 1/2  sayısının karekökünü verir.
+
+Math.LN2       	Bu özellik matematikteki 2 sayısının logaritmasını verir.
+
+Math.LN10      	Bu özellik matematikteki 10 sayısının karekökünü verir.
+
+Math.LOG2E     	Tabanı 2 olan e sabitinin logaritamsını verir.
+
+Math.LOG10E   	Tabanı 10 olan e sabitinin logaritmasını verir. */
