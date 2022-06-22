2293af5903b7483aeaa68cf973727338247b8e40
Nurcan Cürebal <nurcancurebal@hotmail.com>
2022-06-22 19:55:26 +0300

    .

---
+++ sets-and-maps/map-values-four.js
@@ -0,0 +1,3 @@
+const map = new Map([["a",1],["b",2],["c",3]]);
+
+console.log(map.values());  // [Map Iterator] { 1, 2, 3 }
---
+++ sets-and-maps/set-get-function-cagirma-two.js
@@ -0,0 +1,16 @@
+const f = () => {
+    return "Hello, World";
+};
+
+const map = new Map();
+
+map.set(f, "merhaba dünya");
+
+console.log(map.get(f));    // merhaba dünya
+
+
+let mapTwo= new Map();
+
+mapTwo.set(f,f);
+
+console.log(mapTwo.get(f));  // [Function: f]
