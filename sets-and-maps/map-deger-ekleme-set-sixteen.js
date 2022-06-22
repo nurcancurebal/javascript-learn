c160bcdf08bb72829c84cb26f5b372f9271190cb
Nurcan Cürebal <nurcancurebal@hotmail.com>
2022-06-22 23:36:19 +0300

    .

---
+++ sets-and-maps/map-deger-ekleme-set-sixteen.js
@@ -0,0 +1,15 @@
+const countriesMap = new Map();
+
+countriesMap.set('Finland', 'Helsinki');
+countriesMap.set('Sweden', 'Stockholm');
+countriesMap.set('Norway', 'Oslo');
+
+console.log(countriesMap);
+
+console.log(countriesMap.size); // 3
+
+// Map(3) {
+//     'Finland' => 'Helsinki',
+//     'Sweden' => 'Stockholm',
+//     'Norway' => 'Oslo'
+//   }
---
+++ sets-and-maps/mapten-bir-deger-alma-get-seventeen.js
@@ -0,0 +1,10 @@
+countries = [
+    ['Finland', 'Helsinki'],
+    ['Sweden', 'Stockholm'],
+    ['Norway', 'Oslo'],
+];
+
+const map = new Map(countries);
+
+console.log(map.get('Finland'));    // Helsinki
+
