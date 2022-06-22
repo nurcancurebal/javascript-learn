80a3b4fee3d19ec8291c9f8ef526046adee073b5
Nurcan Cürebal <nurcancurebal@hotmail.com>
2022-06-22 23:26:21 +0300

    .

---
+++ sets-and-maps/arrayden-map-olusturma-fifteen.js
@@ -0,0 +1,17 @@
+countries = [
+    ['Finland', 'Helsinki'],
+    ['Sweden', 'Stockholm'],
+    ['Norway', 'Oslo'],
+];
+
+const map = new Map(countries);
+
+console.log(map);
+
+console.log(map.size);  // 3
+
+// Map(3) {
+//     'Finland' => 'Helsinki',
+//     'Sweden' => 'Stockholm',
+//     'Norway' => 'Oslo'
+//   }
---
+++ sets-and-maps/map-fourteen.js
@@ -0,0 +1,5 @@
+// Boş bir Harita(map) Oluşturma
+
+const map = new Map();
+
+console.log(map);   // Map(0) {}
