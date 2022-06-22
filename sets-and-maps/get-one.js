f48b8f4a7c4a611f39675075da65181354869696
Nurcan Cürebal <nurcancurebal@hotmail.com>
2022-06-22 19:17:26 +0300

    .

--- sets-and-maps/get-one.js
+++ sets-and-maps/get-one.js
@@ -1,7 +1,15 @@
-const obj = { a: 1 };
-
 let map = new Map();
 
 map.set("a", "merhaba dünya");
 
 console.log(map.get("a"));  // merhaba dünya
+
+
+
+const obj = { a: 1 };
+
+let mapTwo = new Map();
+
+mapTwo.set(obj, "merhaba, Nurcan");
+
+console.log(mapTwo.get(obj));   // merhaba, Nurcan
