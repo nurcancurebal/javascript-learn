5ce580dab3bca9bf745edd9fe19d7992472bbfff
Nurcan Cürebal <nurcancurebal@hotmail.com>
2022-06-28 18:36:26 +0300

    .

--- destructuring-and-spread/destructuring-object-parameter-ten.js
+++ destructuring-and-spread/destructuring-object-parameter-ten.js
@@ -2,6 +2,7 @@
 
 
 // Tahribatsız nesne parametresi
+
 const rect = {
     width: 20,
     height: 10
@@ -12,46 +13,15 @@ const calculatePerimeter = function (rectangle) {
     return 2 * (rectangle.width + rectangle.height);
 };
 
-console.log(calculatePerimeter(rect));  // 60 (yıkım(destructuring) ile)
+console.log(calculatePerimeter(rect));  // 60 
 
 
 
+// İmha etme ile nesne parametresi
 
-const person = {
-    firstName: 'Nurcan',
-    lastName: 'Cürebal',
-    age: 27,
-    country: 'Canada',
-    job: 'Instructor and Developer',
-    skills: [
-        'HTML',
-        'CSS',
-        'JavaScript',
-        'React',
-        'Redux',
-        'Node',
-        'MongoDB',
-        'Python',
-        'D3.js'
-    ],
-    languages: ['Turkish', 'English', 'German']
+const calculatePerimeterTwo = function ({ width, height }) {
+
+    return 2 * (width + height);
 };
 
-// İmha etmeden kişi nesnesi hakkında bilgi veren bir fonksiyon oluşturalım.
-
-const getPersonInfo = function (obj) {
-
-    const skills = obj.skills;
-    const formattedSkills = skills.slice(0, -1).join(', ');
-    const languages = obj.languages;
-    const formattedLanguages = languages.slice(0, -1).join(', ');
-
-    personInfo = `${obj.firstName} ${obj.lastName} lives in ${obj.country}. She is  ${obj.age} years old. She is an ${obj.job}. She teaches ${formattedSkills} and ${skills[skills.length - 1]}. She speaks ${formattedLanguages} and a little bit of ${languages[2]}.`;
-
-    return personInfo;
-};
-
-console.log(getPersonInfo(person));
-
-// Nurcan Cürebal lives in Canada. She is  27 years old. She is an Instructor and Developer. She teaches HTML, CSS, JavaScript, React,
-// Redux, Node, MongoDB, Python and D3.js. She speaks Turkish, English and a little bit of German.
+console.log(calculatePerimeterTwo(rect));
