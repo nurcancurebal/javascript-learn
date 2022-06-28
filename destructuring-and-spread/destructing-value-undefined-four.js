177875e66dcb86ae05ad8e13f3540327e69ae417
Nurcan Cürebal <nurcancurebal@hotmail.com>
2022-06-28 14:55:20 +0300

    .

--- destructuring-and-spread/destructing-value-undefined-four.js
+++ destructuring-and-spread/destructing-value-undefined-four.js
@@ -1,2 +1,12 @@
 // Dizinin değerinin o dizin için tanımsız olması durumunda varsayılan değeri kullanabiliriz
 
+const names = [undefined, "Nurcan", "Erdinç"];
+
+let [
+    firstPerson = "Bal",
+    secondPerson,
+    thirdPerson,
+    fourthPerson = "Cürebal"
+] = names;
+
+console.log(firstPerson, secondPerson, thirdPerson, fourthPerson);  // Bal Nurcan Erdinç Cürebal
---
+++ destructuring-and-spread/destructuring-spread-operator-five.js
@@ -0,0 +1,2 @@
+// Dizideki tüm elemanlara değişken atayamayız. İlk birkaçını yok edebiliriz ve kalanını spread operatörü(...) kullanarak dizi olarak alabiliriz.
+
