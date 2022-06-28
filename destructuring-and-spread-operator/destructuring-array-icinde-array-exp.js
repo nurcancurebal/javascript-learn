b93e0f8694ae60df3987b653faf21976ddb52822
Nurcan Cürebal <nurcancurebal@hotmail.com>
2022-06-29 00:25:55 +0300

    .

---
+++ destructuring-and-spread-operator/destructuring-array-icinde-array-exp.js
@@ -0,0 +1,8 @@
+// öğrenci adını, becerilerini ve puanını, okunması kolay olmayan diziler dizisinde yapılandırır.Bir satırda aşağıdaki dizi adını ada,
+// beceri dizisini becerilere, puan dizisini puanlara, JavaScript puanını jsScore'a ve React puanını tepkiScore değişkenine bir
+// satırda yok edin(destructuring).
+
+const student = ['Nurcan', ['HTM', 'CSS', 'JS', 'React'], [98, 85, 90, 95]];
+
+
+
