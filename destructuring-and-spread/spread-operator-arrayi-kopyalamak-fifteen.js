cde152f743c5f0ea1ff3590bab324208adde3c90
Nurcan Cürebal <nurcancurebal@hotmail.com>
2022-06-28 20:43:57 +0300

    .

---
+++ destructuring-and-spread/spread-operator-arrayi-kopyalamak-fifteen.js
@@ -0,0 +1,15 @@
+const evens = [0, 2, 4, 6, 8, 10];
+const evensNumbers = [...evens];
+
+console.log(evensNumbers);  // [ 0, 2, 4, 6, 8, 10 ]
+
+
+const odds = [1, 3, 5, 7, 9];
+const oddsNumbers = [...odds];
+
+console.log(oddsNumbers);   // [ 1, 3, 5, 7, 9 ]
+
+
+const wholeNumbers = [...evens, ...odds];
+
+console.log(wholeNumbers);  // [ 0, 2, 4, 6, 8, 10, 1, 3, 5, 7, 9 ]
