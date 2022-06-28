035ae36077cfbbde9780e11b17075583bfdd29f8
Nurcan Cürebal <nurcancurebal@hotmail.com>
2022-06-28 18:25:46 +0300

    .

--- destructuring-and-spread/destructuring-object-parameter-ten.js
+++ destructuring-and-spread/destructuring-object-parameter-ten.js
@@ -42,6 +42,16 @@ const person = {
 const getPersonInfo = function (obj) {
 
     const skills = obj.skills;
-    const formattedSkills = skills.slice(0,-1).join(', ');
-    const languages = 
-}
+    const formattedSkills = skills.slice(0, -1).join(', ');
+    const languages = obj.languages;
+    const formattedLanguages = languages.slice(0, -1).join(', ');
+
+    personInfo = `${obj.firstName} ${obj.lastName} lives in ${obj.country}. She is  ${obj.age} years old. She is an ${obj.job}. She teaches ${formattedSkills} and ${skills[skills.length - 1]}. She speaks ${formattedLanguages} and a little bit of ${languages[2]}.`;
+
+    return personInfo;
+};
+
+console.log(getPersonInfo(person));
+
+// Nurcan Cürebal lives in Canada. She is  27 years old. She is an Instructor and Developer. She teaches HTML, CSS, JavaScript, React,
+// Redux, Node, MongoDB, Python and D3.js. She speaks Turkish, English and a little bit of German.
