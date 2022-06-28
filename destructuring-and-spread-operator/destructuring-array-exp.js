a9c21e9e070f1e63e641a7927c62b122c9a0d527
Nurcan Cürebal <nurcancurebal@hotmail.com>
2022-06-28 21:41:20 +0300

    .

---
+++ destructuring-and-spread-operator/destructuring-array-exp.js
@@ -0,0 +1,12 @@
+const constants = [2.72, 3.14, 9.81, 37, 100];
+
+const [e, pi, gravity, humanBodyTemp, waterBoilingTemp] = constants;
+
+console.log(e, pi, gravity, humanBodyTemp, waterBoilingTemp);   // 2.72 3.14 9.81 37 100
+
+
+const countries = ['Finland', 'Estonia', 'Sweden', 'Denmark', 'Norway'];
+
+const [fin, est, sw, den, nor] = countries;
+
+console.log(fin, est, sw, den, nor);    // Finland Estonia Sweden Denmark Norway
