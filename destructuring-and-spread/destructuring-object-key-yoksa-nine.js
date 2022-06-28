1ebaf1939f523050b6d3f33d68a3a68a9c699c04
Nurcan Cürebal <nurcancurebal@hotmail.com>
2022-06-28 16:07:05 +0300

    .

--- destructuring-and-spread/destructuring-object-key-yoksa-nine.js
+++ destructuring-and-spread/destructuring-object-key-yoksa-nine.js
@@ -8,3 +8,19 @@ const rectangle = {
     area: 200
 };
 
+let { width, height, area, perimeter = 80 } = rectangle;
+
+console.log(width, height, area, perimeter);    // 20 10 200 80
+
+
+
+const rectangleTwo = {
+    widthTwo: 15,
+    heightTwo: 5,
+    areaTwo: 75,
+    perimeterTwo: 100
+};
+
+let { widthTwo, heightTwo, areaTwo, perimeterTwo = 99 } = rectangleTwo;
+
+console.log(widthTwo, heightTwo, areaTwo, perimeterTwo);    // 15 5 75 100
