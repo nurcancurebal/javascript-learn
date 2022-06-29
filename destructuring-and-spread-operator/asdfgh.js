d6b3ce53084f320cb483cdafe07512995caa8e59
Nurcan Cürebal <nurcancurebal@hotmail.com>
2022-06-29 20:42:07 +0300

    .

---
+++ destructuring-and-spread-operator/asdfgh.js
@@ -0,0 +1,28 @@
+const students = [
+    ['David', ['HTM', 'CSS', 'JS', 'React'], [98, 85, 90, 95]],
+    ['John', ['HTM', 'CSS', 'JS', 'React'], [85, 80, 85, 80]]
+];
+
+console.log(convertArrayToObject(students));
+
+
+
+
+function convertArrayToObject(array) {
+
+
+}
+
+
+/* [
+    {
+      name: 'David',
+      skills: ['HTM','CSS','JS','React'],
+      scores: [98,85,90,95]
+    },
+    {
+      name: 'John',
+      skills: ['HTM','CSS','JS','React'],
+      scores: [85, 80,85,80]
+    }
+  ] */
--- destructuring-and-spread-operator/destructuring-and-spread-array-icinde-array-exp.js
+++ destructuring-and-spread-operator/destructuring-and-spread-array-icinde-array-exp.js
@@ -2,9 +2,9 @@ const student = ['Nurcan', ['HTML', 'CSS', 'JS', 'React'], [98, 85, 90, 95]];
 
 let [name, skills, score] = student;
 
-const scoreNew = [...score];
+const scoreSpread = [...score];
 
-let jsScore = scoreNew[2];
-let reactScore = scoreNew[3];
+let jsScore = scoreSpread[2];
+let reactScore = scoreSpread[3];
 
 console.log(name, skills, jsScore, reactScore);   // Nurcan [ 'HTML', 'CSS', 'JS', 'React' ] 90 95
