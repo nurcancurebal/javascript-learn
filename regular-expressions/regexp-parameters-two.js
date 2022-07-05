978cea247b921eca59f51d6ea70c967b4ae67783
Nurcan Cürebal <nurcancurebal@hotmail.com>
2022-07-05 16:26:18 +0300

    .

--- regular-expressions/regexp-parameters-two.js
+++ regular-expressions/regexp-parameters-two.js
@@ -1,10 +1,10 @@
-// Bir normal ifade iki parametre alır. Gerekli bir arama modeli ve isteğe bağlı bir bayrak.
+// Bir düzenli ifade(regular expression) iki parametre alır. Gerekli bir arama modeli ve isteğe bağlı bir bayrak.
 
 // Pattern(Model): Bir kalıp(desen), bir metin veya bir tür benzerlik içeren herhangi bir kalıp(desen) olabilir. Örneğin, bir e-postadaki
 // istenmeyen e-posta kelimesi, bir e-postada aramak istediğimiz bir kalıp olabilir veya bir telefon numarası biçimindeki
 // numara bizim ilgi alanımız olabilir.
 
-// Flags(Bayraklar): normal bir ifadede arama türünü belirleyen isteğe bağlı parametrelerdir. Bazı bayrakları görelim:
+// Flags(Bayraklar): düzenli bir ifadede (regular expression) arama türünü belirleyen isteğe bağlı parametrelerdir. Bazı bayrakları görelim:
 
 /* g: tüm metinde bir kalıp aramak anlamına gelen küresel bir bayrak
    i: büyük/küçük harfe duyarsız bayrak (hem küçük hem de büyük harf arar)
@@ -13,8 +13,10 @@
 // RegExp Constructor ile bir desen oluşturma
 
 
-// Genel bayrak ve büyük/küçük harfe duyarlı olmayan bayrak olmadan normal ifade bildirme.
+// Genel bayrak ve büyük/küçük harfe duyarlı olmayan bayrak olmadan düzenli ifade(regular expression) bildirme.
 
 let pattern = 'love';
 
 let regEx = new RegExp(pattern);
+
+// Genel bayrak ve büyük/küçük harfe duyarsız bayrakla düzenli ifade(regular expression) bildirme.
--- regular-expressions/regular-exressions-one.js
+++ regular-expressions/regular-exressions-one.js
@@ -1,7 +1,6 @@
-// Normal bir ifade veya RegExp, verilerde model(kalıp) bulmaya yardımcı olan küçük bir programlama dilidir. Bir RegExp, farklı veri türlerinde
+// Düzenli bir ifade(regular expression) veya RegExp, verilerde model(kalıp) bulmaya yardımcı olan küçük bir programlama dilidir. Bir RegExp, farklı veri türlerinde
 // bir modelin olup olmadığını kontrol etmek için kullanılabilir. JavaScript'te RegExp'i kullanmak için ya RegExp yapıcısını
 // kullanırız ya da iki eğik çizgi ve ardından bir bayrak kullanarak bir RegExp modeli(kalıbını) ilan edebiliriz. Bir deseni iki şekilde oluşturabiliriz.
 
-// Bir dize bildirmek için tek bir tırnak işareti kullanırız, bir normal ifadeyi bildirmek için çift tırnak işareti kullanırız, iki eğik
+// Bir dize bildirmek için tek bir tırnak işareti kullanırız, bir düzenli ifadeyi(regular expression) bildirmek için çift tırnak işareti kullanırız, iki eğik
 // çizgi ve isteğe bağlı bir bayrak kullanırız. Bayrak g, i, m, s, u veya y olabilir.
-
