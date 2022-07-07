4b67fd936f5374ae8922b1de213376d60def0057
Nurcan Cürebal <nurcancurebal@hotmail.com>
2022-07-07 21:58:24 +0300

    .

--- regular-expressions/negatif-pozitif-sayilarla-islem-yapma-exp.js
+++ regular-expressions/negatif-pozitif-sayilarla-islem-yapma-exp.js
@@ -1,6 +1,6 @@
 const txt= ' Bazı parçacıkların yatay x ekseni üzerindeki konumu -12, -4, -3 ve -1 negatif yönde, 0 orijinde, 4 ve 8 pozitif yönde';
 
-const points = /^-\d/g;
+const points = /[-]*\d/g;   // [-]: başında - olanları al; *: sıfır veya daha fazla kez; \d: sayıları tek tek al
 
 const matches = txt.match(points);
 
