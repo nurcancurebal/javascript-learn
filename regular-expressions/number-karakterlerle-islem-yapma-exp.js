08922df5fd1959ad75def0f634e75d3b570bffd6
Nurcan Cürebal <nurcancurebal@hotmail.com>
2022-07-07 20:25:47 +0300

    .

--- regular-expressions/number-karakterlerle-islem-yapma-exp.js
+++ regular-expressions/number-karakterlerle-islem-yapma-exp.js
@@ -2,6 +2,14 @@ const txt = 'Aylik maaştan 4000 euro, yillik 10000 euro ikramiye, ayda 5500 eur
 
 const pattern = /\d+/g; // sayının hepsini aldık
 
-const match = txt.match(pattern);
+const match = txt.match(pattern);   // (3) ['4000', '10000', '5500'] //! array olarak aldı ancak arrayin içindekiler string
 
-console.log(match);
+let result = match[0] * 12;
+
+result += Number(match[1]);
+
+let resultTwo = match[2] * 12;
+
+result += resultTwo;
+
+console.log(`Kişinin yillik geliri: ${result}`);
