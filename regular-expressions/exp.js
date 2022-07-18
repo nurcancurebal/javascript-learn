63c65214d6de89f6770f7a753f211b59568e4172
Nurcan Cürebal <nurcancurebal@hotmail.com>
2022-07-18 14:52:51 +0300

    .

---
+++ regular-expressions/exp.js
@@ -0,0 +1,18 @@
+// Metni temizleyen bir fonksiyon yazınız. Aşağıdaki metni temizleyin. Temizledikten sonra, dizede en sık kullanılan üç kelimeyi sayın.
+
+let sentence = `%I $am@% a %tea@cher%, &and& I lo%#ve %tea@ching%;. There $is nothing; &as& mo@re rewarding as educa@ting &and& @emp%o@wering peo@ple. ;I found tea@ching m%o@re interesting tha@n any other %jo@bs. %Do@es thi%s mo@tivate yo@u to be a tea@cher!?`;
+
+console.log(cleanText(sentence));
+
+// I am a teacher and I love teaching There is nothing as more rewarding as educating and empowering people I found teaching more interesting than any other jobs Does this motivate you to be a teacher
+
+console.log(mostFrequentWords(cleanedText));
+
+// [{word:'I', count:3}, {word:'teaching', count:2}, {word:'teacher', count:2}]
+
+
+function cleanText(text) {
+    
+
+    
+}
