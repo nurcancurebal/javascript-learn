a9745213e2c642764aa539a6dceb8c2a83cd5224
Nurcan Cürebal <nurcancurebal@hotmail.com>
2022-07-18 19:48:44 +0300

    .

---
+++ console-object-methods/console-info-six.js
@@ -0,0 +1,7 @@
+// Tarayıcı konsolunda bilgi mesajı görüntüler.
+
+console.info('30 Days Of JavaScript challenge is trending on Github'); // 30 Days Of JavaScript challenge is trending on Github
+
+console.info('30 Days Of fullStack challenge might be released'); // 30 Days Of fullStack challenge might be released
+
+console.info('30 Days Of HTML and CSS challenge might be released'); // 30 Days Of HTML and CSS challenge might be released
