2118d0c55395d1a52f2ae648164ff39ef086a7a9
Nurcan Cürebal <nurcancurebal@hotmail.com>
2022-07-18 23:38:16 +0300

    .

---
+++ console-object-methods/assert-exp.js
@@ -0,0 +1,1 @@
+console.assert(10 > 2 * 10, '2 çarpı 10 büyüktür 10\' dan');  // Assertion failed: 2 çarpı 10 büyüktür 10' dan
---
+++ console-object-methods/gruplama-exp.js
@@ -0,0 +1,66 @@
+const users = [
+    {
+        name: 'Nurcan',
+        title: 'Programmer',
+        country: 'Canada',
+        city: 'Ottava',
+        age: 27
+    },
+    {
+        name: 'Erdinç',
+        title: 'Teacher',
+        country: 'Sweden',
+        city: 'London',
+        age: 25
+    },
+    {
+        name: 'Bal',
+        title: 'Instructor',
+        country: 'Norway',
+        city: 'Oslo',
+        age: 22
+    },
+    {
+        name: 'Timur',
+        title: 'Developer',
+        country: 'Denmark',
+        city: 'Copenhagen',
+        age: 28
+    }
+];
+
+console.group('Users');
+console.log(users);
+console.groupEnd();
+
+// Users
+//   [
+//     {
+//       name: 'Nurcan',
+//       title: 'Programmer',
+//       country: 'Canada',
+//       city: 'Ottava',
+//       age: 27
+//     },
+//     {
+//       name: 'Erdinç',
+//       title: 'Teacher',
+//       country: 'Sweden',
+//       city: 'London',
+//       age: 25
+//     },
+//     {
+//       name: 'Bal',
+//       title: 'Instructor',
+//       country: 'Norway',
+//       city: 'Oslo',
+//       age: 22
+//     },
+//     {
+//       name: 'Timur',
+//       title: 'Developer',
+//       country: 'Denmark',
+//       city: 'Copenhagen',
+//       age: 28
+//     }
+//   ]
