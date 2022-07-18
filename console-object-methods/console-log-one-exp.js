c0e5465b5dd4aa3d484a593a908de870b331f3e5
Nurcan Cürebal <nurcancurebal@hotmail.com>
2022-07-18 16:56:55 +0300

    .

---
+++ console-object-methods/console-log-one-exp.js
@@ -0,0 +1,16 @@
+// Bu bölümde konsol ve konsol nesne yöntemlerini ele alacağız. Mutlak yeni başlayanlar genellikle hangisini kullanacaklarını bilmezler:
+// console.log(), document.write() veya document.getElementById.
+
+// Tarayıcı konsolunda çıktıyı göstermek için konsol nesne yöntemlerini kullanıyoruz ve çıktıyı tarayıcı belgesinde (görüntüleme portu) 
+// göstermek için document.write kullanıyoruz. Her iki yöntem de yalnızca test etme ve hata ayıklama amacıyla kullanılır. Konsol yöntemi,
+// tarayıcıdaki en popüler test ve hata ayıklama aracıdır. DOM ile etkileşim kurmak istediğimizde, JavaScript kullanmayı denediğimizde
+// document.getElementById() kullanırız. DOM'u başka bir bölümde ele alacağız.
+
+// Konsol, ünlü konsol.log() yöntemine ek olarak, başka yöntemler de sağlar.
+
+// console.log()
+// Tarayıcı konsolunda çıktıyı göstermek için console.log() kullanıyoruz. Değerleri değiştirebiliriz ve ayrıca %c kullanarak oturumu
+// kapatma stilini belirleyebiliriz.
+
+
+
