5c6b89eea749590426145b5a484edf47133726ec
Nurcan Cürebal <nurcancurebal@hotmail.com>
2022-07-19 23:47:19 +0300

    .

--- error-handling/throw-five.js
+++ error-handling/throw-five.js
@@ -3,3 +3,39 @@
 // Aşağıdakilerin her biri bir özel durum oluşturur:
 
 
+throw 'Error2'; // bir dize değeriyle bir istisna oluşturur
+throw 42;       // 42 değeriyle bir istisna oluşturur
+throw true;     // true değeriyle bir istisna oluşturur
+throw new Error('Required'); // Gerekli mesajıyla bir hata nesnesi oluşturur
+
+
+
+const throwErrorExampleFun = () => {
+
+    let message;
+
+    let x = prompt('Enter a number: ');
+
+    try {
+
+        if (x == '') throw 'empty';
+
+        if (isNaN(x)) throw 'not a number';
+
+        x = Number(x);
+
+        if (x < 5) throw 'too low';
+
+        if (x > 10) throw 'too high';
+
+    } catch (err) {
+
+        console.log(err);
+    };
+};
+
+throwErrorExampleFun();
+
+// let x = prompt('Enter a number: ');
+//             ^
+// ReferenceError: prompt is not defined
