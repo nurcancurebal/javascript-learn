2c0cd729483483a15e96f936a55f7b6e3f43ad1c
Nurcan Cürebal <nurcancurebal@hotmail.com>
2022-07-22 02:31:20 +0300

    .

---
+++ window-methods/window-alert-method.js
@@ -0,0 +1,9 @@
+// En başta gördüğünüz gibi alert() metodu, belirtilen bir mesaj ve bir OK butonu ile bir uyarı kutusu görüntüler. Yerleşik bir
+// yöntemdir ve argüman alır.
+
+
+alert(message);
+
+alert('Welcome to 30DaysOfJavaScript');
+
+// Yıkıcı ve can sıkıcı olduğu için çok fazla uyarı kullanmayın, sadece test etmek için kullanın.
