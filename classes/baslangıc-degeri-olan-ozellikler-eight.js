83b5037e1f75b6655af68c3aa33cf14507a0fb0f
Nurcan Cürebal <nurcancurebal@hotmail.com>
2022-07-21 17:53:37 +0300

    .

--- classes/baslangıc-degeri-olan-ozellikler-eight.js
+++ classes/baslangıc-degeri-olan-ozellikler-eight.js
@@ -3,4 +3,56 @@
 // becerisine sahip olabiliriz ve bir süre sonra bir beceri kazanacağız.
 
 
+class Person {
 
+    constructor(firstName, lastName, age, country, city) {
+
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+        this.country = country;
+        this.city = city;
+        this.score = 0;
+        this.skills = [];
+    };
+    getFullName() {
+
+        const fullName = this.firstName + " " + this.lastName;
+        return fullName;
+    };
+};
+
+const person1 = new Person("Nurcan", "Cürebal", "27", "Canada", "Ottava");
+const person2 = new Person("Erdinç", "Cürebal", "27", "Turkey", "Istanbul");
+
+console.log(person1);
+console.log(person1.score);
+console.log(person1.skills);
+
+console.log(person2);
+console.log(person2.score);
+console.log(person2.skills);
+
+
+// Person {
+//     firstName: 'Nurcan',
+//     lastName: 'Cürebal',
+//     age: '27',
+//     country: 'Canada',
+//     city: 'Ottava',
+//     score: 0,
+//     skills: []
+//   }
+//   0
+//   []
+//   Person {
+//     firstName: 'Erdinç',
+//     lastName: 'Cürebal',
+//     age: '27',
+//     country: 'Turkey',
+//     city: 'Istanbul',
+//     score: 0,
+//     skills: []
+//   }
+//   0
+//   []
---
+++ classes/getter-nine.js
@@ -0,0 +1,8 @@
+// Bir yöntem, normal bir yöntem veya bir alıcı veya bir ayarlayıcı olabilir. Bir görelim, alıcı(getter) ve ayarlayıcı(setter).
+
+// Get yöntemi, nesneden değere erişmemize izin verir. Get anahtar sözcüğünü ve ardından bir işlev(function) kullanarak bir get yöntemi
+// yazıyoruz. Özelliklere doğrudan nesneden erişmek yerine, değeri elde etmek için alıcıyı kullanırız.
+
+
+
+
