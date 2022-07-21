9134e8da24b52ae3faae63f8c69374fbf6714176
Nurcan Cürebal <nurcancurebal@hotmail.com>
2022-07-22 01:20:00 +0300

    .

--- string-methods/string-floata-cevir-parsefloat.js
+++
@@ -1,9 +0,0 @@
-// string to float
-// buradada string veri tipini değiştiriyoruz ancak 5.5 gibi rasyonel sayılarda parseınt() kullanılmaz
-// parseFloat() kullanılır
-
-let bbb = '7,5';
-
-let cc = parseFloat(bbb);
-
-console.log(typeof cc); // çıktı number
---
+++ veri-turunu-degistirme-casting/string-floata-cevir-parsefloat-number-plussign.js
@@ -0,0 +1,27 @@
+// string to float
+// buradada string veri tipini değiştiriyoruz ancak 5.5 gibi rasyonel sayılarda parseınt() kullanılmaz
+
+// parseFloat()
+// Number()
+// Plus sign(+) kullanılır
+
+let bbb = '7,5';
+
+let cc = parseFloat(bbb);
+
+console.log(typeof cc); // çıktı number
+
+
+
+let num = '9.81';
+
+let numFloat = Number(num);
+
+console.log(numFloat); // 9.81
+
+
+let numTwo = '9.81';
+
+let numFloatTwo = +num;
+
+console.log(numFloatTwo); // 9.81
