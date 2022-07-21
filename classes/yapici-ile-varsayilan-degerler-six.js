93badc5bf44fee4440f9bcb98fa8219a9e05ce90
Nurcan Cürebal <nurcancurebal@hotmail.com>
2022-07-21 16:12:04 +0300

    .

--- classes/yapici-ile-varsayilan-degerler-six.js
+++ classes/yapici-ile-varsayilan-degerler-six.js
@@ -1,2 +1,43 @@
 // Yapıcı işlevi özellikleri, diğer normal işlevler gibi varsayılan bir değere sahip olabilir.
 
+class Person {
+
+    constructor(
+
+        firstName = 'Nurcan',
+        lastName = 'Cürebal',
+        age = 27,
+        country = 'Canada',
+        city = 'Ottava'
+
+    ) {
+
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+        this.country = country;
+        this.city = city;
+    };
+};
+
+const person1 = new Person(); // varsayılan değerleri alacaktır
+const person2 = new Person('Lidiya', 'Tekle', 28, 'Finland', 'Espoo');
+
+console.log(person1);
+console.log(person2);
+
+
+// Person {
+//     firstName: 'Nurcan',
+//     lastName: 'Cürebal',
+//     age: 27,
+//     country: 'Canada',
+//     city: 'Ottava'
+//   }
+//   Person {
+//     firstName: 'Lidiya',
+//     lastName: 'Tekle',
+//     age: 28,
+//     country: 'Finland',
+//     city: 'Espoo'
+//   }
