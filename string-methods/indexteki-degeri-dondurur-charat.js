5c9c5fd4b1214898fb894e067eedb9ff639ee1d6
Nurcan Cürebal <nurcancurebal@hotmail.com>
2022-07-22 00:59:42 +0300

    .

---
+++ string-methods/indexteki-degeri-dondurur-charat.js
@@ -0,0 +1,8 @@
+let string = '30 Days Of JavaScript';
+
+console.log(string.charAt(0));        // 3
+
+
+let lastIndex = string.length - 1;
+
+console.log(string.charAt(lastIndex)); // t
---
+++ string-methods/son-index-konum-dondurur-lasindexof.js
@@ -0,0 +1,9 @@
+let string = 'I love JavaScript. If you do not love JavaScript what else can you love.';
+
+console.log(string.lastIndexOf('love'));       // 67
+
+console.log(string.lastIndexOf('you'));        // 63
+
+console.log(string.lastIndexOf('JavaScript')); // 38
+
+console.log(string.lastIndexOf('are'));       // -1
