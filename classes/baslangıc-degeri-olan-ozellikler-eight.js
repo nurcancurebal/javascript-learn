2e330acadcf6f31e32d3b93ef5bb487a2e49b3c1
Nurcan Cürebal <nurcancurebal@hotmail.com>
2022-07-21 17:39:12 +0300

    .

---
+++ classes/baslangıc-degeri-olan-ozellikler-eight.js
@@ -0,0 +1,6 @@
+// Bazı özellikler için bir sınıf oluşturduğumuzda bir başlangıç değerine sahip olabiliriz. Örneğin bir oyun oynuyorsanız, başlama
+// puanınız sıfır olacaktır. Dolayısıyla, sıfır olan bir başlangıç puanımız veya puanımız olabilir. Başka bir şekilde, bir başlangıç
+// becerisine sahip olabiliriz ve bir süre sonra bir beceri kazanacağız.
+
+
+
