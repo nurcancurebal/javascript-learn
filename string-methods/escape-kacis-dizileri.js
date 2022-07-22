480b76a5410c84f73392a6de38e7be758bf52a7a
Nurcan Cürebal <nurcancurebal@hotmail.com>
2022-07-22 11:53:08 +0300

    .

---
+++ string-methods/escape-kacis-dizileri.js
@@ -0,0 +1,48 @@
+/*
+\n = yeni satıra geçecek
+\t = tab yani 8 karakterlik boşluk bırakır
+\\ = \ işaretini göstermek için 2 tane back slash koyarız ekran çıktısı tek back slash olur
+\' = console.log('Nurcan\'ın kocası') ekran çıktısı Nurcan'ın kocası şeklinde olur
+\" = aynı şekilde tırnak içinde yazdığın cümlede " geçiyorsa bunu \" şeklinde yazarsın */
+
+let yasim = 26;
+
+let adim = "Nurcan Cürebal";
+
+let bilgilerim = "Adım: " + adim + "  \nYaşım: " + yasim;
+
+console.log(bilgilerim);
+
+
+console.log('I hope everyone is enjoying the 30 Days Of JavaScript challenge.\nDo you ?');
+
+console.log('Days\tTopics\tExercises');
+
+console.log('Day 1\t3\t5');
+
+console.log('Day 2\t3\t5');
+
+console.log('Day 3\t3\t5');
+
+console.log('Day 4\t3\t5');
+
+console.log('This is a backslash  symbol (\\)');
+
+console.log('In every programming language it starts with \"Hello, World!\"');
+
+console.log("In every programming language it starts with \'Hello, World!\'");
+
+console.log('The saying \'Seeing is Believing\' isn\'t correct in 2020');
+
+
+// I hope everyone is enjoying the 30 Days Of JavaScript challenge.
+// Do you ?
+// Days   Topics   Exercises
+// Day 1 3 5
+// Day 2 3 5
+// Day 3 3 5
+// Day 4 3 5
+// This is a backslash  symbol (\)
+// In every programming language it starts with "Hello, World!"
+// In every programming language it starts with 'Hello, World!'
+// The saying 'Seeing is Believing' isn't correct in 2020
