928c7b2f096bb246bb98c40415316e8cc4e9d417
Nurcan Cürebal <nurcancurebal@hotmail.com>
2022-07-29 15:27:19 +0300

    .

---
+++ promises-sozler/promise-one.js
@@ -0,0 +1,10 @@
+// Biz insanlar, belirli bir zamanda bir faaliyette bulunma sözü veririz veya alırız. Sözümüzü tutarsak başkalarını mutlu
+// ederiz ama sözümüzü tutmazsak bu durum hoşnutsuzluklara yol açabilir. JavaScript'te söz vermenin yukarıdaki örneklerle
+// ortak bir yanı vardır.
+
+// Bir Söz, JavaScript'te eşzamansız işlemleri işlemenin bir yoludur. Eşzamansız bir eylemin nihai başarı değeri veya
+// başarısızlık nedeni ile işleyicilere izin verir. Bu, eşzamansız yöntemlerin eşzamanlı yöntemler gibi değerler
+// döndürmesine izin verir: eşzamansız yöntem, nihai değeri hemen döndürmek yerine, değeri gelecekte bir noktada sağlama
+// sözü verir.
+
+// Bir Söz(promise) şu durumlardan birindedir:
