60283ea4abf356b681adc4d9d38aad969849a24c
Nurcan Cürebal <nurcancurebal@hotmail.com>
2022-07-29 23:59:58 +0300

    .

--- promises-sozler/async-and-await-eszamansiz-ve-bekleme-five.js
+++ promises-sozler/async-and-await-eszamansiz-ve-bekleme-five.js
@@ -6,3 +6,18 @@ const square = async function (n) {
 };
 
 console.log(square(2)); // Promise { 4 }
+
+// Bir fonksiyonun önündeki zaman uyumsuz kelimesi, fonksiyonun bir söz vereceği anlamına gelir. Yukarıdaki kare fonksiyonu
+// bir değer yerine bir söz verir.
+
+// Sözün değerine nasıl erişiriz? Söz verilen değere erişmek için, wait anahtar sözcüğünü kullanacağız.
+
+const squareTwo = async function (n) {
+
+    return n * n;
+};
+
+const value = await squareTwo(2);
+
+console.log(value);
+
