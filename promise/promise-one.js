ba38c4e57d624b1141b48215ebfc8347d3fad6bb
Nurcan Cürebal <nurcancurebal@hotmail.com>
2022-08-02 01:07:55 +0300

    .

--- promise/promise-one.js
+++ promise/promise-one.js
@@ -18,3 +18,33 @@
 // işlemin tamamlanması ile işleyicilerinin eklenmesi arasında bir yarış koşulu yoktur.)
 
 // Promise.prototype.then() ve Promise.prototype.catch() yöntemleri vaatleri döndürdüğü(geri verdiği) için zincirlenebilirler.
+
+
+main();
+
+async function main() {
+
+    let a = 30;
+
+    let b;
+
+    console.log("PROMISE BASLANGIC");
+
+    b = await new Promise(function (resolve, reject) {
+
+        setTimeout(() => {
+
+            resolve(15);   // resolve bekletme sonucunda 15 vermiş, await' i b ye atadığımız için b' ye 15 verdi
+
+        }, 2000);
+    });
+
+    console.log("PROMISE BITIS");
+
+    console.log(b + a);
+};
+
+// PROMISE BASLANGIC
+// burada 2 sn bekler
+// PROMISE BITIS
+// 45
