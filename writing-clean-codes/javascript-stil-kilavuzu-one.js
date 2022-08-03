22e1063aedc8a1b1382c9f990402e13790da38e2
Nurcan Cürebal <nurcancurebal@hotmail.com>
2022-08-03 18:13:29 +0300

    .

---
+++ writing-clean-codes/javascript-stil-kilavuzu-one.js
@@ -0,0 +1,27 @@
+// JavaScript stil kılavuzu, JavaScript kodunun nasıl yazılması ve düzenlenmesi gerektiğini söyleyen bir standartlar
+// dizisidir. Bu bölümde JavaScript rehberleri ve temiz kod nasıl yazılır hakkında konuşacağız.
+
+// JavaScript bir programlama dilidir ve insan dili gibi söz dizimi vardır. JavaScript'in sözdizimi, ikna ve basitlik adına
+// belirli bir stil kılavuzuna göre yazılmalıdır.
+
+// Stil kılavuzuna neden ihtiyacımız var?
+// Çok uzun zamandır tek başına kod yazıyorsun ama artık takım halinde çalışıyor gibi görünüyor. Çalıştığı sürece size kod
+// yazmanız önemli değil, ancak bir projede ve aynı kod tabanında 10 veya 20 veya daha fazla geliştiriciden oluşan bir
+// ekiple çalıştığınızda, kod dağınık ve yönetimi zor olacaktır. uyulması gereken herhangi bir yönerge yoktur.
+
+// Kendi yönergelerinizi ve kurallarınızı geliştirebilir veya iyi geliştirilmiş yönergeleri de uyarlayabilirsiniz. En yaygın
+// bilinen yönergeleri bize bildirin. En yaygın JavaScript Stil Kılavuzları
+
+// .Airbnb JavaScript Stil Kılavuzu
+// .JavaScript Standart Stil Kılavuzu
+// .Google JavaScript Stil Kılavuzu
+
+// 1.Airbnb JavaScript Stil Kılavuzu(Airbnb JavaScript Style Guide)
+// Airbnb, internetteki en popüler JavaScript stil kılavuzlarından birine sahiptir. JavaScript'in neredeyse her yönünü de
+// kapsar ve birçok geliştirici ve şirket tarafından benimsenmiştir. Airbnb stil kılavuzunu inceleyebilirsiniz. ben de
+// denemenizi tavsiye ederim. Stillerinin kullanımı çok kolay ve anlaşılması kolaydır.
+
+// 2.Standart JavaScript Stil Kılavuzu(Standard JavaScript Style Guide)
+// Bu kılavuz Airbnb kadar popüler değil ama bakmaya değer. Stil kılavuzlarından noktalı virgülü kaldırdılar.
+
+// 3.Google JavaScript Stil Kılavuzu(Google JavaScript Style Guide)
