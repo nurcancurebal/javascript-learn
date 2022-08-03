f53d0b92caf86243397cca780934160c55230a18
Nurcan Cürebal <nurcancurebal@hotmail.com>
2022-08-03 19:46:47 +0300

    .

---
+++ writing-clean-codes/classes-siniflar-nine.js
@@ -0,0 +1,20 @@
+// söz dizimi
+class ClassName {
+
+    // kod buraya gelecek
+};
+
+// sınıf tanımlama
+class Person {
+    constructor(firstName, lastName) {
+
+        console.log(this); // Çıktıyı buradan kontrol edin
+
+        this.firstName = firstName;
+        this.lastName = lastName;
+    };
+};
+
+// Takip ettiğiniz stil kılavuzu ne olursa olsun tutarlı olun. Bazı programlama paradigmalarını ve tasarım modellerini takip
+// edin. Unutmayın, kodunuzu belirli bir düzende veya şekilde yazmazsanız, kodunuzu okumak zor olacaktır. Bu nedenle,
+// okunabilir kod yazarak kendiniz veya kodunuzu okuyacak biri için bir iyilik yapın.
