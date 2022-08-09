164f0800441e48cd441dff82e42bf03878f95088
Nurcan Cürebal <nurcancurebal@hotmail.com>
2022-08-09 19:49:44 +0300

    .

--- operators-operatorler/arithmetic-operators.js
+++
@@ -1,6 +0,0 @@
-// Addition(+): a + b
-// Subtraction(-): a - b
-// Multiplication(*): a * b
-// Division(/): a / b
-// Modulus(%): a % b    (kalan bulma)
-// Exponential(**): a ** b  (Üssünü alma)
--- operators-operatorler/assignments-atama-operators.js
+++ operators-operatorler/assignments-atama-operators.js
@@ -1,14 +1,8 @@
-/*assignments operators(atama operatörleri)
-
-
-Addition(+): a + b
-Subtraction(-): a - b
-Multiplication(*): a * b
-Division(/): a / b
-Modulus(%): a % b
-Exponential(**): a ** b
+/*
+assignments operators(atama operatörleri)
 
 operatörler     kullanım şekli       description
+=                x = y                x = y
 +=               x += y               x = x+y
 -=               x -= y               x = x-y
 *=               x *= y               x = x*y
@@ -17,33 +11,3 @@ operatörler     kullanım şekli       description
 **=              x ** y               x = X**y (üssünü alma)
 
 */
-
-
-const PI = 3.14;
-
-let radius = 100;          // metre cinsinden uzunluk
-
-const areaOfCircle = PI * radius * radius;  //Bir dairenin alanını hesaplayalım
-
-console.log(areaOfCircle);  //  314 m
-
-
-const gravity = 9.81;      // in m/s2
-
-let mass = 72;             // in Kilogram
-
-const weight = mass * gravity;  // Bir cismin ağırlığını hesaplayalım
-
-console.log(weight);        // 706.32 N(Newton)
-
-
-const boilingPoint = 100;  // oC cinsinden sıcaklık, suyun kaynama noktası
-
-const bodyTemp = 37;       // oC cinsinden vücut ısısı
-
-// Dize enterpolasyonu kullanarak dizeyi sayılarla birleştirme
-// Suyun kaynama noktası 100 oC'dir.İnsan vücut sıcaklığı 37 oC'dir.Dünyanın yerçekimi 9.81 m
-
-console.log(
-    `The boiling point of water is ${boilingPoint} oC.\nHuman body temperature is ${bodyTemp} oC.\nThe gravity of earth is ${gravity} m / s2.`
-);
