cb686a08bf1421723f95a9f5ded339c2519c5478
Nurcan Cürebal <nurcancurebal@hotmail.com>
2022-08-11 13:33:58 +0300

    .

--- conditionals-sartlilar/if-else-ternary-exp.js
+++ conditionals-sartlilar/if-else-ternary-exp.js
@@ -36,33 +36,3 @@ if (a > b) {
 
     console.log(b + ' is greater than ' + a);
 };
-
-
-// ternary
-
-
-let age = 27;
-let ageTwo = 13;
-
-console.log(age > 18 ? 'You are old enough to drive.' : 'You are left with ' + (18 - age) + ' years to drive.');
-
-console.log(ageTwo > 18 ? ' You are old enough to drive.' : 'You are left with ' + (18 - ageTwo) + ' years to drive.');
-
-
-
-let one = 9;
-let two = 45;
-
-console.log(one > two ? one + ' büyüktür ' + two : two + ' büyüktür ' + one);
-
-
-
-let first = 25;
-let second = 30;
-
-let conclusionFirst = first % 2 == 0;
-let conclusionSecond = second % 2 == 0;
-
-console.log(conclusionFirst ? first + 'is an even number' : first + ' is an odd number.');
-
-console.log(conclusionSecond ? second + ' is an even number' : second + ' is is an odd number.');
