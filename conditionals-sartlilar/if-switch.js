9ac6961c152dc376a8c174c3cb382b319c688c5c
Nurcan Cürebal <nurcancurebal@hotmail.com>
2022-08-11 13:37:45 +0300

    .

--- conditionals-sartlilar/if-switch.js
+++ conditionals-sartlilar/if-switch.js
@@ -18,37 +18,3 @@ if (num2 > 0) {
 };
 
 //  2' den fazla koşula ihtiyacımız olursa else if kullanırız
-
-
-/* 
-switch(caseValue){
-    case 1:
-      // code
-      break
-    case 2:
-     // code
-     break
-    case 3:
-     // code
-     break
-    default:
-     // code
-  } */
-
-
-let weather = 'yagmurlu';
-
-switch (weather) {
-
-    case 'gunesli':
-        console.log('hava güneşli');
-        break;
-
-    case 'bulutlu':
-        console.log('ceketini al');
-        break;
-
-    default:
-        console.log('yağmurluk giy');
-
-};
---
+++ conditionals-sartlilar/switch-case.js
@@ -0,0 +1,33 @@
+/* 
+switch(caseValue){
+    case 1:
+      // code
+      break
+    case 2:
+     // code
+     break
+    case 3:
+     // code
+     break
+    default:
+     // code
+} 
+*/
+
+
+let weather = 'yagmurlu';
+
+switch (weather) {
+
+    case 'gunesli':
+        console.log('hava güneşli');
+        break;
+
+    case 'bulutlu':
+        console.log('ceketini al');
+        break;
+
+    default:
+        console.log('yağmurluk giy');
+
+};
