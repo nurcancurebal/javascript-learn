37cd834b53ab2c4597a148783571c20756c49e80
Nurcan Cürebal <nurcancurebal@hotmail.com>
2022-08-24 17:13:05 +0300

    .

--- objects-nesneler/yapici-new-operatoru.js
+++ objects-nesneler/yapici-new-operatoru.js
@@ -1,5 +1,6 @@
 /* -> Yapıcı, "new" operatörü
-{ ... } yazımı bir objenin yaratılmasına yarar.Fakat bir objenin benzeri farklı objeler yaratmak istenebilir.Örneğin farklı kullanıcılar, farklı menü değerleri.
+{ ... } yazımı bir objenin yaratılmasına yarar.Fakat bir objenin benzeri farklı objeler yaratmak istenebilir.Örneğin farklı
+kullanıcılar, farklı menü değerleri.
 Bu yapıcı fonksiyon ve "new" operatörü ile yapılabilir. */
 
 
@@ -17,7 +18,7 @@ function Kullanici(isim) {
 
 let kullanici = new Kullanici("Nurcan");
 
-console.log(kullanici);
+console.log(kullanici); // Kullanici { isim: 'Nurcan', yoneticiMi: false }
 console.log(kullanici.isim); // Nurcan
 console.log(kullanici.yoneticiMi); // false
 
@@ -40,39 +41,45 @@ function Kullanici2(isim) {
 
 // Öyleyse new Kullanici2(“Nurcan”) objesi aşağıdaki gibidir:
 
-let Kullanici2 = {
+/* let Kullanici2 = {
 
-    isim: "Erdinç",
+    isim: "Nurcan",
     yoneticiMi: false
-};
+}; */
 
-/* Eğer başka bir kullanici oluşturmak istiyorsanız, yapmanız gereken new Kullanici("Macide"), new Kullanici("Muzaffer") gibi kullanmaktır.Doğrudan her defasında obje tanımlamaktan daha kısa ve anlaşılması kolaydır.
+/* Eğer başka bir kullanici oluşturmak istiyorsanız, yapmanız gereken new Kullanici("Macide"), new Kullanici("Muzaffer")
+gibi kullanmaktır.Doğrudan her defasında obje tanımlamaktan daha kısa ve anlaşılması kolaydır.
 Yapıcı fonksiyonların amacı – tekrar kullanılabilecek objeleri yaratan kodun uygulanmasıdır.
-Dikkat edecek olursanız, herhangi bir fonksiyon yapıcı fonksiyon olabilir.Her fonksiyon new ile çalıştırılabilir, ve yukarda anlatılan algoritmaya göre çalışır. “Yapıcı fonksiyon isimleri büyük harfle başlamalıdır” aslında genel bir ittifaktır, bunu daha da açıklayıcı yapmak için bu fonksiyonlar new ile çağırılmalıdır. */
+Dikkat edecek olursanız, herhangi bir fonksiyon yapıcı fonksiyon olabilir.Her fonksiyon new ile çalıştırılabilir, ve
+yukarda anlatılan algoritmaya göre çalışır. “Yapıcı fonksiyon isimleri büyük harfle başlamalıdır” aslında genel bir
+ittifaktır, bunu daha da açıklayıcı yapmak için bu fonksiyonlar new ile çağırılmalıdır. */
 
 new function () {
     //…
 }
 
-/* Eğer birçok satırdan oluşan kodda amacınız sadece karmaşık bir obje yapmak ise, bunları yapıcı fonksiyon içine aşağıdaki gibi almak mümkündür: */
+/* Eğer birçok satırdan oluşan kodda amacınız sadece karmaşık bir obje yapmak ise, bunları yapıcı fonksiyon içine aşağıdaki
+gibi almak mümkündür: */
 
 let kullanici3 = new function () {
 
     this.isim = "Bal";
-    this.yonetici = false;
+    this.yonetici = true;
 
     // diğer karmaşık yapılar
     // mantıklar veya yerel değişkenler
 };
 
-/* Yapıcı fonksiyon tekrar çağırılamaz çünkü hiçbir yere kayıt edilmemiştir, sadece yaratılır ve çağırılır.Böylece yapıcı metod ilerde tekrar kullanılmayacağına garanti verir. */
+/* Yapıcı fonksiyon tekrar çağırılamaz çünkü hiçbir yere kayıt edilmemiştir, sadece yaratılır ve çağırılır.Böylece yapıcı
+metod ilerde tekrar kullanılmayacağına garanti verir. */
 
 
 /* -> Yapıcı modu testi: new.target
 İleri düzey konular
 Bu olay çok nadir kullanılır.Eğer her şeyi öğrenmek istemiyorsanız burayı geçebilirsiniz. */
 
-/* Fonksiyon içinde, bu fonksiyon new ile mi yoksa new olmadan mı çağırılmış bu new.target özelliği kullanılarak anlaşılabilir.
+/* Fonksiyon içinde, bu fonksiyon new ile mi yoksa new olmadan mı çağırılmış bu new.target özelliği kullanılarak
+anlaşılabilir.
 Normal çağrılarda bunun içerisi boştur fakat new ile çağrılırsa: */
 
 function Kullanici4() {
@@ -81,16 +88,16 @@ function Kullanici4() {
 }
 
 // new kullanılmadan:
-User(); // new.target undefined döndürür.
+Kullanici4(); // undefined, new.target undefined döndürür.
 
 // new kullanılarak:
-new User(); // function Kullanici4 { ... } gibi ekrana fonksiyonu yazar
+new Kullanici4(); // [Function: Kullanici4], gibi ekrana fonksiyonu yazar
 
 // new ve normal sözdiziminin(syntax) aynı çalışmasını sağlar:
 
 function Kullanici5(isim) {
 
-    if (!new.target) { // eğer çart yerine getirilmezse
+    if (!new.target) { // eğer şart yerine getirilmezse
 
         return new Kullanici5(isim); // ...yeni birisi eklenir.
     }
@@ -98,15 +105,18 @@ function Kullanici5(isim) {
     this.isim = isim;
 }
 
-let timur = Kullanici5("Figen"); // çağrıyı new Kullanici5(isim) fonksiyonuna yönlendirir.
+let figen = Kullanici5("Figen"); // çağrıyı new Kullanici5(isim) fonksiyonuna yönlendirir.
 
-console.log(timur.isim); // timur
+console.log(figen.isim); // Figen
 
-/* Bu yaklaşım bazı kütüphanelerde yazımı daha esnek yapabilmek amacıyla kullanılır.Her yerde kullanılması o kadar da iyi değildir.Çünkü new ne olup bittiği hakkında bilgi vermektedir.new ile yeni bir obje yaratıldığını anlayabiliyorsunuz ki bu da iyi bir şeydir. */
+/* Bu yaklaşım bazı kütüphanelerde yazımı daha esnek yapabilmek amacıyla kullanılır.Her yerde kullanılması o kadar da iyi
+değildir.Çünkü new ne olup bittiği hakkında bilgi vermektedir.new ile yeni bir obje yaratıldığını anlayabiliyorsunuz ki bu
+da iyi bir şeydir. */
 
 
 /* -> Yapıcı metodun return sözcüğü
-Genelde yapıcı metodların return sözcüğü yoktur.Amaçları gerekli olan bilgileri this içine yazmaktır ve bu da otomatik olarak sonuçtur.
+Genelde yapıcı metodların return sözcüğü yoktur.Amaçları gerekli olan bilgileri this içine yazmaktır ve bu da otomatik
+olarak sonuçtur.
 Fakat return sözcüğü var ise kurallar basittir:
 -- Eğer return obje ile çağırılırsa this yerine bu obje döndürülür.
 -- Eğer return ilkel bir obje ile çağırılırsa görmezden gelinir.
@@ -148,26 +158,25 @@ let kullanici7 = new Kullanici();
 
 
 /* -> Yapıcı içerisinde metod
-Yapıcı fonksiyon kullanmak objeye mükemmel esneklik sağlar.Yapıcı fonksiyon parametreleri tanımlar ve objenin nasıl yapılacağını, ne konulması gerektiğini belirtir.
+Yapıcı fonksiyon kullanmak objeye mükemmel esneklik sağlar.Yapıcı fonksiyon parametreleri tanımlar ve objenin nasıl
+yapılacağını, ne konulması gerektiğini belirtir.
 Tabiki this'e sadece özelliklerde değil metodlar içerisinde de ekleme yapılabilir.
-Örneğin, aşağıdaki new User(isim) verilen isim ile yeni bir obje oluşturur.Bu obje selamVer metoduna sahiptir. */
+Örneğin, aşağıdaki new Kullanici8(isim) verilen isim ile yeni bir obje oluşturur.Bu obje selamVer metoduna sahiptir. */
 
-function Kullanici(isim) {
+function Kullanici8(isim) {
 
     this.isim = isim;
 
     this.selamVer = function () {
+
         console.log("Benim adım: " + this.isim);
     };
 }
 
-let ihsan = new User("İhsan");
+let timur = new Kullanici8("Timur");
 
-ihsan.selamVer(); // Benim adım: İhsan
+timur.selamVer(); // Benim adım: Timur
 
+console.log(timur); // Kullanici8 { isim: 'Timur', selamVer: [Function (anonymous)] }
 
-/* ihsan = {
-   name: "İhsan",
-   selamVer: function() { ... }
-}
-*/
+console.log(Kullanici8()); // undefined, direk ulaşılamıyor fonksiyona
