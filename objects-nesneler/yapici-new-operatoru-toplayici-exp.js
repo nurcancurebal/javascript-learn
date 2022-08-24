b295c990cf5b5cb05888d82af6667ba0c0ca930e
Nurcan Cürebal <nurcancurebal@hotmail.com>
2022-08-24 19:04:18 +0300

    .

--- objects-nesneler/yapici-new-operatoru-toplayici-exp.js
+++ objects-nesneler/yapici-new-operatoru-toplayici-exp.js
@@ -6,7 +6,24 @@ oku() metodu kendimiz bilgi gireceğiz ve bunu deger'e eklemelidir.
 Diğer bir deyişle deger özelliği tüm kullanıcıların girdiği değerlerin baslangicDegeri ile toplamıdır.
 Aşağıda bir demosunu görmektesiniz: */
 
-let toplayici = new Toplayici(1); // toplayıcıya  1 ile başla.
-toplayici.oku(5); // kullanıcının girdiği değeri toplar.
-toplayici.oku(11); // kullanıcının girdiği değeri toplar.
-console.log(toplayici.deger); // toplamı gösterir.
+/* let toplayici1 = new Toplayici1(1); // toplayıcıya  1 ile başla.
+toplayici1.oku(4); // kullanıcının girdiği değeri toplar.
+toplayici1.oku(11); // kullanıcının girdiği değeri toplar.
+console.log(toplayici1.deger); // toplamı gösterir. */
+
+
+function Toplayici(baslangicDegeri) {
+
+    this.deger = baslangicDegeri;
+
+    this.oku = function (a) {
+
+        this.deger += a;
+    };
+}
+
+let toplayici = new Toplayici(1);
+
+toplayici.oku(4);
+toplayici.oku(11);
+console.log(toplayici.deger); // 16
