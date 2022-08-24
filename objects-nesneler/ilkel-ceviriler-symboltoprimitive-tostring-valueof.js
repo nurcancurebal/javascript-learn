c9f246fba361fdbb9fa53a2e2d2deeb047fb17e8
Nurcan Cürebal <nurcancurebal@hotmail.com>
2022-08-24 15:00:56 +0300

    .

--- objects-nesneler/ilkel-ceviriler-symboltoprimitive-tostring-valueof.js
+++ objects-nesneler/ilkel-ceviriler-symboltoprimitive-tostring-valueof.js
@@ -1,16 +1,22 @@
 /* -> Objelerin ilkel çevirileri
 Objeler obj1 + obj2 gibi toplanırsa, obj1 - obj2 gibi çıkarılırsa veya console.log(obj) gibi yazdırılırsa ne olur?
 Objeler içerisinde bu çevirimi yapan özel metodlar bulunmaktadır.
-Tip Dönüşümleri bölümünde sayısal, karakter ve boolean çevrimleri gösterildi. Fakat objeler için daha sonra değilineceği söylendi. Şimdi objeler ve semboller hakkında bilginiz olduğuna göre bunu anlaması daha da kolay olacaktır.
-Objeler için boolean çevirimi bulunmamaktadır çünkü tüm objeler boolean dahilinde true'dur. Bundan dolayı sadece sayısal ve karakter çevrimleri mevcuttur.
-Sayısal çevirim obje çıkarıldığında veya metematiksel fonksiyonlar uygulandığında meydana gelir. Örneğin Date objesi (Tarih ve Zaman bölümünde anlatılacak) çıkarılabilir ve date1-date2 bu iki tarih arasındaki zaman farkını verir.
+Tip Dönüşümleri bölümünde sayısal, karakter ve boolean çevrimleri gösterildi. Fakat objeler için daha sonra değilineceği
+söylendi. Şimdi objeler ve semboller hakkında bilginiz olduğuna göre bunu anlaması daha da kolay olacaktır.
+Objeler için boolean çevirimi bulunmamaktadır çünkü tüm objeler boolean dahilinde true'dur. Bundan dolayı sadece sayısal ve
+karakter çevrimleri mevcuttur.
+Sayısal çevirim obje çıkarıldığında veya metematiksel fonksiyonlar uygulandığında meydana gelir. Örneğin Date objesi (Tarih
+ve Zaman bölümünde anlatılacak) çıkarılabilir ve date1-date2 bu iki tarih arasındaki zaman farkını verir.
 Karakter dizisi(string) çevirimi için – genelde console.log(obj) çağırıldığında meydana gelir. */
 
+
 /* -> ToPrimitive(ipucu(hint) de denebilir)
-Objenin ilkel bir tip olarak gerekli olduğu durumlarda, örneğin console.log veya matematiksel uygulamalarda, ToPrimitive algoritması kullanılarak bu işlem yapılır. (Özellikleri)
+Objenin ilkel bir tip olarak gerekli olduğu durumlarda, örneğin console.log veya matematiksel uygulamalarda, ToPrimitive
+algoritması kullanılarak bu işlem yapılır. (Özellikleri)
 Bu algoritma özel bir obje metodu ile ilkel tipe çevrimi düzenlememizi sağlar.
 Duruma bağlı olarak, bu çevirime “ipucu(hint)” da denir.
 Üç çeşidi vardır: */
+
 
 /* -> "karakter dizisi" – string
 Uygulama karakter dizisi bekliyorsa, objeden karakter dizisine çeviri yapmak gerekmektedir. Örneğin console.log: */
@@ -21,8 +27,9 @@ console.log(obj);
 // veya obje özellik anahtarı olarak kullanıldığında.
 digerObj[obj] = 123;
 
+
 /* -> "sayı" – number
-Eğer bir uygulama sayı bekliyorsa, objeden sayıya çevrilmelidir. Genelde aşağıdaki gibi matematiksel işlemlerde kullanılır: */
+Eğer bir uygulama sayı bekliyorsa, objeden sayıya çevrilmelidir. Genelde aşağıdaki gibi matematiksel işlemlerde kullanılır*/
 
 // doğrudan çeviri
 let num = Number(obj);
@@ -34,9 +41,11 @@ let delta = tarih1 - tarih2;
 // karşılaştırmalarda
 let greater = kullanici1 > kullanici2;
 
+
 /* -> "varsayılan" – default
 Çok nadir olmakla birlikte operatör ne beklediği konusunda “emin olmayabilir”
-Örneğin, binary + karakter dizisi için birleştirme işlemi yaparken sayı için toplama işlemi yapar, bundan dolayı hem karakter hem sayı olabilir. Veya objeyi karakter, sayı veya sembol ile == şeklinde karşılaştırırken */
+Örneğin, binary + karakter dizisi için birleştirme işlemi yaparken sayı için toplama işlemi yapar, bundan dolayı hem
+karakter hem sayı olabilir. Veya objeyi karakter, sayı veya sembol ile == şeklinde karşılaştırırken */
 
 // binary toplama
 let toplam = araba1 + araba2;
@@ -46,16 +55,19 @@ if (kullanici3 == 1) {
   //... 
 };
 
-/* Büyüktür/küçüktür operatörü <> karakter ve sayı ile çalışabilir. Fakat yine de default değil de sayı ipucunu kullanır. Bu tarihsel nedenlerden ötürü böyledir.
+/* Büyüktür/küçüktür operatörü <> karakter ve sayı ile çalışabilir. Fakat yine de default değil de sayı ipucunu kullanır.
+Bu tarihsel nedenlerden ötürü böyledir.
 Pratikte tüm objeler ( Date objesi hariç ) "default" çevrimini “number” çevrimi ile aynı şekilde yaparlar. */
 
-/* Sadece üç çeşit ipucu(hint) bulunmaktadır. boolean bulunmamaktadır, çünkü her obje zaten boolean true döndürür. "default" ve "number" için de aynı olduklarını varsayarsanız, sadece iki çevrim bulunmaktadır.
+/* Sadece üç çeşit ipucu(hint) bulunmaktadır. boolean bulunmamaktadır, çünkü her obje zaten boolean true döndürür.
+"default" ve "number" için de aynı olduklarını varsayarsanız, sadece iki çevrim bulunmaktadır.
 Çevrimi yapabilmek için JavaScript üç obje metodu bulup çağırmaya çalışır"
 Eğer obj[Symbol.toPrimitive](hint) metodu varsa çağır,
 Eğer yoksa ve ipucu(hint) karakter ise
 obj.toString() ve obj.valueOf() metodlardan hangisi varsa çalıştır.
 Eğer ipucu(hint) "number" veya "default" ise
 obj.valueOf() ve obj.toString() metodlarından hangisi varsa çalıştır. */
+
 
 /* -> Symbol.toPrimitive
 Symbol.toPrimitive adında bir metod var ve bu metod çeviri metodunu adlandırmak için kullanılır. Örneğin: */
@@ -89,10 +101,13 @@ console.log(kullanici4);
 console.log(+kullanici4); // ipucu: number 1000
 console.log(kullanici4 + 500); // ipucu: default 1500
 
-/* Koddan da gördüğünüz üzere kullanici çeviriye göre karakter veya para olabiliyor. Tek bir metod kullanici[Symbol.toPrimitive] tüm çeviri durumlarını karşılıyor. */
+/* Koddan da gördüğünüz üzere kullanici çeviriye göre karakter veya para olabiliyor. Tek bir metod
+kullanici[Symbol.toPrimitive] tüm çeviri durumlarını karşılıyor. */
+
 
 /* -> toString/valueOf
-toString ve valueOf gibi metodlar ilk JavaScript çıktığı zamandan kalma metodlardır. Sembol değillerdir( O zamanlar sembol yoktu), yani normal metodlardır. Eski tip çevirim düzenlemesi yapabilmenize olanak verir.
+toString ve valueOf gibi metodlar ilk JavaScript çıktığı zamandan kalma metodlardır. Sembol değillerdir( O zamanlar sembol
+yoktu), yani normal metodlardır. Eski tip çevirim düzenlemesi yapabilmenize olanak verir.
 Eğer Symbol.toPrimitive yoksa JavaScript aşağıdaki metodları bulmaya çalışır:
 toString ->> valueOf “string” ipuçları için.
 valueOf ->> toString diğer hallerde.
@@ -142,9 +157,12 @@ console.log(kullanici6 + 500); // Bal500
 
 // Eğer Symbol.toPrimitive ve valueOf yoksa toString metodu objeleri ilkel tiplere çevirmeye çalışır.
 
+
 /* -> Dönüş Tipleri
-İlkel tip çevrilerinde bilinmesi gereken en önemli olay bu metodların illa da ipucunda alınan tiplere dönüştürmesine gerek yoktur.
-toString() illa karakter döndürecek diye veya Symbol.toPrimitive içerisinde eğer ipucu sayı ise sayı döndürecek diye bir kural yoktur.
+İlkel tip çevrilerinde bilinmesi gereken en önemli olay bu metodların illa da ipucunda alınan tiplere dönüştürmesine gerek
+yoktur.
+toString() illa karakter döndürecek diye veya Symbol.toPrimitive içerisinde eğer ipucu sayı ise sayı döndürecek diye bir
+kural yoktur.
 Tek zorunlu nokta bu metodların ilkel tip döndürmesidir
 Bu ilkel değeri alan operatör işine devam eder ve eğer gerekliyse başka çeviriler de yapabilir.
 Örneğin:Matematiksel operatörler( binary toplama dışında) ToNumber çevrimini kullanır: */
@@ -184,6 +202,8 @@ let obj4 = {
 
 console.log(obj4 + 2); // 3, (ToPrimitive boolean dönderdi, string değil => ToNumber)
 
+
 /* -> Tarihi Nedenlar
-toString veya valueOf ilkel tip döndürmelidir. Eğer obje dönderiyorsa bir hata vardır. Fakat bu obje sadece pas geçilir aynı metodu olmadığında sadece pas geçildiği gibi.
+toString veya valueOf ilkel tip döndürmelidir. Eğer obje dönderiyorsa bir hata vardır. Fakat bu obje sadece pas geçilir
+aynı metodu olmadığında sadece pas geçildiği gibi.
 Buna karşın Symbol.toPrimitive ilkel tip döndürmek zorundadır, diğer türlü hata meydana gelir. */
