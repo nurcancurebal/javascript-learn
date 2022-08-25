9009e939057d74988c1896effe9f32b48d3421a2
Nurcan Cürebal <nurcancurebal@hotmail.com>
2022-08-25 21:01:44 +0300

    .

--- string-methods-dize-yontemleri/hangi-indexte-oldugunu-bulma-indexof.js
+++ string-methods-dize-yontemleri/hangi-indexte-oldugunu-bulma-indexof.js
@@ -11,3 +11,44 @@ console.log(string.indexOf('days')); //ekran çıktısı= -1
 //içine yazdığın şeyin nerede olduğunu söyler hiç yoksa -1 döner
 //IndexOf() ilk bulduğunun nerede olduğunu döndürür
 //lastIndexOf() = son bulduğunun nerede olduğunu döndürür
+
+
+/* İsteğe bağlı olan ikinci parametre aramaya nereden başlanacağının belirtilmesine yarar.
+Örneğin "an"'ın ilk bulunduğu pozisyon 8'dir. Bir sonraki denk gelişi ise 19. pozisyonda olur. */
+
+let str = "N`aber Canım - Tavşan";
+
+console.log(str.indexOf('an', 9)); // 19
+
+/* Eğer sizin istediğiniz tüm tekrarlar ise, indexOf'u döngü içerisinde kullanabilirsiniz. Her yeni çağrı bir önceki pozisyonu
+tutar: */
+
+let str2 = 'Bir berber bir berbere gel birader beraber bir berber dükkanı açalım demiş';
+
+let hedef2 = 'bir';
+
+let poz2 = 0;
+
+while (true) {
+
+    let bulunanPoz = str2.indexOf(hedef2, poz2);
+
+    if (bulunanPoz == -1) break;
+
+    console.log(`Bulunan poz: ${bulunanPoz}`);
+
+    poz2 = bulunanPoz + 1; // bir sonraki pozisyondan aramaya devam et.
+}
+
+// Aynı algoritma aşağıdaki şekilde daha kısa bir biçimde yazılabilir:
+
+let str3 = 'Bir berber bir berbere gel birader beraber bir berber dükkanı açalım demiş';
+
+let hedef3 = "bir";
+
+let poz3 = -1;
+
+while ((poz3 = str3.indexOf(hedef3, poz3 + 1)) != -1) {
+
+    console.log(poz3);
+}
