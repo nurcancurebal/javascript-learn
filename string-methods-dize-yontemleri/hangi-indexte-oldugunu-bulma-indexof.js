77c7a729b1ae244f84ef6411a545393dc47272db
Nurcan Cürebal <nurcancurebal@hotmail.com>
2022-08-25 21:27:03 +0300

    .

--- string-methods-dize-yontemleri/hangi-indexte-oldugunu-bulma-indexof.js
+++ string-methods-dize-yontemleri/hangi-indexte-oldugunu-bulma-indexof.js
@@ -20,8 +20,9 @@ let str = "N`aber Canım - Tavşan";
 
 console.log(str.indexOf('an', 9)); // 19
 
-/* Eğer sizin istediğiniz tüm tekrarlar ise, indexOf'u döngü içerisinde kullanabilirsiniz. Her yeni çağrı bir önceki pozisyonu
-tutar: */
+
+/* Eğer sizin istediğiniz tüm tekrarlar ise, indexOf'u döngü içerisinde kullanabilirsiniz. Her yeni çağrı bir önceki
+pozisyonu tutar: */
 
 let str2 = 'Bir berber bir berbere gel birader beraber bir berber dükkanı açalım demiş';
 
@@ -40,15 +41,25 @@ while (true) {
     poz2 = bulunanPoz + 1; // bir sonraki pozisyondan aramaya devam et.
 }
 
+/*
+Bulunan poz: 11
+Bulunan poz: 27
+Bulunan poz: 43 */
+
 // Aynı algoritma aşağıdaki şekilde daha kısa bir biçimde yazılabilir:
 
 let str3 = 'Bir berber bir berbere gel birader beraber bir berber dükkanı açalım demiş';
 
-let hedef3 = "bir";
+let hedef3 = 'bir';
 
 let poz3 = -1;
 
-while ((poz3 = str3.indexOf(hedef3, poz3 + 1)) != -1) {
+while ((poz3 = str3.indexOf(hedef3, poz3 + 1)) != -1) { // TODO
 
     console.log(poz3);
 }
+
+/* 
+11
+27
+43 */
