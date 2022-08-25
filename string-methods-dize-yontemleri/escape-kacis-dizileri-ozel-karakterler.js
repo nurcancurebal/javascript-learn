cc438bfb95f424e85f950a8acc9df71367988da9
Nurcan Cürebal <nurcancurebal@hotmail.com>
2022-08-25 20:44:22 +0300

    .

--- string-methods-dize-yontemleri/escape-kacis-dizileri-ozel-karakterler.js
+++ string-methods-dize-yontemleri/escape-kacis-dizileri-ozel-karakterler.js
@@ -3,7 +3,25 @@
 \t = tab yani 8 karakterlik boşluk bırakır
 \\ = \ işaretini göstermek için 2 tane back slash koyarız ekran çıktısı tek back slash olur
 \' = console.log('Nurcan\'ın kocası') ekran çıktısı Nurcan'ın kocası şeklinde olur
-\" = aynı şekilde tırnak içinde yazdığın cümlede " geçiyorsa bunu \" şeklinde yazarsın */
+\" = aynı şekilde tırnak içinde yazdığın cümlede " geçiyorsa bunu \" şeklinde yazarsın
+\b = Backspace(geri tuşu)
+\f = Form feed(form besleme)
+\r = Carriage return(satır başı)
+\uNNNN = hex kodu ile bir unicode NNNN, örneğin \u00A9 – © kopyalama hakkı için kullanılan işaret. Kesinlikle 4 basamaklı
+hex değeri olmalıdır.
+\u{NNNNNNNN} = Bazı karakterler nadirde olsa iki unicode sembolü ile ifade edilirler. 4 bytten oluşan uzun bir yazımı
+vardır. Karakterlerin süslü parantez içine alınması gerekmektedir. */
+
+/* Tüm özel karakterler her zaman \ karakteri ile başlarlar. Karakterler normal akışında giderken başka bir iş yapması için
+var olan işlemi kesmesinden dolayı “kesme karakteri” denebilir… */
+
+
+// Unicode örnekleri:
+
+console.log("\u00A9"); // ©
+console.log("\u{20331}"); // 佫, Uzun bir çince hiyerograf (uzun unicode)
+console.log("\u{1F60D}"); // 😍, gülen yüz sembolü (uzun unicode)
+
 
 let yasim = 26;
 
--- string-methods-dize-yontemleri/karakter-uzunlugu-length.js
+++ string-methods-dize-yontemleri/karakter-uzunlugu-length.js
@@ -1,5 +1,16 @@
+/* -> length bir özelliktir
+Genelde başka diller ile çalışmış programcılar yanlışlıkla str.length yerine str.length() metodunu çağırmaktadırlar. Böyle
+bir metod yoktur.
+
+str.length sayısal bir değerdir, fonksiyon değildir! Sonunda parantez açıp kapanmaz. */
+
 //length= kaç karakter uzunluğunda olduğunu söylüyor
 
 let js = 'javascript';
 
-console.log(js.length);
+console.log(js.length); // 10
+
+
+console.log(`Naber\n`.length); // 6
+
+// Dikkat ederseniz \n “özel karakter” oludğundan dolayı bir karakter olarak tanımlandı.
