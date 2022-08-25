2e7ce56c6a06643187ad41d3cf28f31625dcc4f0
Nurcan Cürebal <nurcancurebal@hotmail.com>
2022-08-26 00:14:03 +0300

    .

--- string-methods-dize-yontemleri/parcalara-ayirip-arraye-cevirmek-split.js
+++ string-methods-dize-yontemleri/parcalara-ayirip-arraye-cevirmek-split.js
@@ -1,15 +1,16 @@
 // Karakter dizisini belirtilen karakterden parçalara ayırarak bir dizi oluşturur.
+
 // split(',',3) virgülden ayırır 3 tane array oluşturur gerisini almaz
 
 
-let adSoyad = "Nurcan Cürebal";
+let adSoyad = "Nurcan Cürebal"; // boşluktan parçalama
 
 let result = adSoyad.split(" ");
 
 console.log(result); // [ 'Nurcan', 'Cürebal' ]
 
 
-let str = "Maz,da,Op,el,To,yo,ta";
+let str = "Maz,da,Op,el,To,yo,ta"; // virgülden parçalama
 
 let resulTwo = str.split(",");
 
@@ -18,6 +19,6 @@ console.log(resulTwo);  // [ 'Maz', 'da', 'Op', 'el', 'To', 'yo', 'ta' ]
 
 let js = 'JavaScript';
 
-const charsInJavaScript = js.split(''); // harf harf parçaladık
+const charsInJavaScript = js.split(''); // harf harf parçalama
 
 console.log(charsInJavaScript); // ["J", "a", "v", "a", "S", "c", "r", "i", "p", "t"]
