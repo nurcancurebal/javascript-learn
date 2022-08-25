d95acde651344c0a6374513742ea415f69024168
Nurcan Cürebal <nurcancurebal@hotmail.com>
2022-08-25 20:32:32 +0300

    .

---
+++ string-methods-dize-yontemleri/strings.js
@@ -0,0 +1,3 @@
+/* -> Karakter Dizisi - Strings
+JavaScript metinsel değerleri karakter dizisi olarak tutar. Bir karakter ile ( char ) karakter dizisi ( string ) arasında bir fark yoktur.
+Karakter dizisi formatı her zaman UTF-16’dır ve sayfanın karakter setine bağlı değildir. */
