23fd61de8e36d0eeeaf22f060e8b213040505dfb
Nurcan Cürebal <nurcancurebal@hotmail.com>
2022-08-27 00:00:03 +0300

    .

---
+++ string-methods-dize-yontemleri/isaretleri-cikarma-exp.js
@@ -0,0 +1,14 @@
+/* Para işaretlerini metinden çıkarın
+Formda "$120" ücret bulunmaktadır. Önce dolar işareti ardından sayı gelmektedir.
+
+Öyle bir fonksiyon yazın ki extractCurrencyValue(str) bu fonksiyon sadece sayısal değeri döndersin. */
+
+
+console.log(extractCurrencyValue('$120') === 120); // true
+
+// Testler ile korunaklı olan aç.
+
+function extractCurrencyValue(str) {
+
+    return +str.slice(1);
+}
