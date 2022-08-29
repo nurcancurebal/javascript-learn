2f67587ab1e1fb96f89132a2bc699f8b49551705
Nurcan Curebal <nurcancurebal@hotmail.com>
2022-08-30 00:27:32 +0300

    .

--- arrays-diziler/map-split-slice-join-exp.js
+++ arrays-diziler/map-split-slice-join-exp.js
@@ -12,12 +12,16 @@ Not: split kullanarak karakterleri dizi haline getirebilirsiniz, bunu join ile t
 
 function camelize(str) {
 
-    return str
-      .split('-') // splits 'my-long-word' into array ['my', 'long', 'word']
-      .map(
-        // capitalizes first letters of all array items except the first one
-        // converts ['my', 'long', 'word'] into ['my', 'Long', 'Word']
-        (word, index) => index == 0 ? word : word[0].toUpperCase() + word.slice(1)
-      )
-      .join(''); // joins ['my', 'Long', 'Word'] into 'myLongWord'
-  }
+  return str
+    .split('-') // splits 'my-long-word' into array ['my', 'long', 'word']
+    .map(
+      // capitalizes first letters of all array items except the first one
+      // converts ['my', 'long', 'word'] into ['my', 'Long', 'Word']
+      (word, index) => index == 0 ? word : word[0].toUpperCase() + word.slice(1)
+    )
+    .join(''); // joins ['my', 'Long', 'Word'] into 'myLongWord'
+}
+
+console.log(camelize("background-color")); // backgroundColor
+console.log(camelize("list-style-image")); // listStyleImage
+console.log(camelize("-webkit-transition")); // WebkitTransition
