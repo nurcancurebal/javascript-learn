b8dc53a1495e177f095746d2c2a65e404ad8b279
Nurcan Curebal <nurcancurebal@hotmail.com>
2022-08-30 22:15:36 +0300

    .

--- arrays-diziler/belirli-araligi-alma-slice-example.js
+++ arrays-diziler/belirli-araligi-alma-slice-example.js
@@ -1,8 +1,16 @@
 //Slice() yöntemi, bir dizinin bir bölümünün sığ bir kopyasını, baştan sona seçilen (bitiş dahil değildir) yeni
 //bir dizi nesnesine döndürür; burada başlangıç ​​ve bitiş, o dizideki öğelerin dizinini temsil eder.Orijinal dizi değişmez.
 
-let itCompanies = ['Facebook', 'Google', 'Microsoft', 'Apple', 'IBM', 'Oracle', 'Amazon'];
+// slice() fonksiyonunu kullanarak bir kopyasını yaratabilir ve sonrasında bunu sıralayabilirsiniz:
 
-let itCompaniesFour = itCompanies.slice(2, 4);
+function copySorted(arr) {
 
-console.log(itCompaniesFour);   // [ 'Microsoft', 'Apple' ]
+    return arr.slice().sort();
+}
+
+let arr = ["HTML", "JavaScript", "CSS"];
+
+let sorted = copySorted(arr);
+
+console.log(sorted); // [ 'CSS', 'HTML', 'JavaScript' ]
+console.log(arr); // [ 'HTML', 'JavaScript', 'CSS' ]
---
+++ arrays-diziler/kopyala-sirala-exp.js
@@ -0,0 +1,17 @@
+/* Karakterlerden oluşan bir arr disini, kopyalayan ve sıralayan fakat arr de bir değişikliğe neden olmayan fonksiyonu yazınız.
+
+Bu kopyayı döndüren copySorted(arr) fonksiyonunu yazınız. */
+
+/* let arr = ["HTML", "JavaScript", "CSS"];
+
+let sorted = copySorted(arr);
+
+console.log(sorted); // CSS, HTML, JavaScript
+console.log(arr); // HTML, JavaScript, CSS (değişmedi) */
+
+
+let arr = ["HTML", "JavaScript", "CSS"];
+
+function copySorted(arr) {
+    
+}
