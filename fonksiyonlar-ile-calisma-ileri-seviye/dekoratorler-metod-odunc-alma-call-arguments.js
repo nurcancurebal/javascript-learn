694ae99df16c4a33efe17cc07a9bac63abe763b3
Nurcan Curebal <nurcancurebal@hotmail.com>
2022-09-12 21:00:47 +0300

    .

--- fonksiyonlar-ile-calisma-ileri-seviye/dekoratorler-metod-odunc-alma-call-arguments.js
+++ fonksiyonlar-ile-calisma-ileri-seviye/dekoratorler-metod-odunc-alma-call-arguments.js
@@ -1,47 +1,49 @@
 /* -> Metod Ödünç Alma
 Hash fonksiyonunda biraz değişiklik yapalım: */
 
-function hash(args) {
+function hash1(args) {
 
     return args[0] + ',' + args[1];
 }
 
+console.log(hash1('Erdinç', 'Nurcan','Bal')); // E,r
+
 /* Bundan sonra bu fonksiyon sadece iki argümanla çalışacak. Bunun yerine belirsiz sayıdaki argümanla çalışsa daha iyi olur.
 
-Bunu kullanmanın doğal yolu arr.join metodudur: */
+Bunu kullanmanın doğal yolu arr.join metodudur(join: arrayi stringe çevir birleştir): */
 
-function hash(args) {
+function hash2(args) {
 
     return args.join();
 }
 
-/* … Malesef bu çalışmaz. Çünkü hash(argümanlar) çağırılmakta ve arguments objei hem döngülenebilir hemde dizi-benzeri olduğundan, fakat gerçek dizi olmadığından çalışmaz.
+console.log(hash2('Erdinç', 'Nurcan','Bal')); // Error: arguments.join fonksiyon değil.
+
+/* … Malesef bu çalışmaz. Çünkü hash(argümanlar) çağırılmakta ve arguments objesi hem döngülenebilir hemde dizi-benzeri olduğundan, fakat gerçek dizi olmadığından çalışmaz.
 
 Öyleyse join bu durumda çağırılamaz, örneğin: */
 
-function hash() {
+function hash3() {
 
     console.log(arguments.join()); // Error: arguments.join fonksiyon değil.
 }
 
-hash(1, 2);
+hash3(1, 2);
 
 // Fakat yine de dizi birleştirme kullanmanın kolay bir yolu vardır.
 
-function hash() {
+function hash4() {
 
     console.log([].join.call(arguments)); // 1,2
 }
 
-hash(1, 2);
+hash4(1, 2);
 
 /* Bu cambazlığa metod ödünç alma denir.
 
 Normal diziden [].join join ödünç alınır. Sonrasında arguments contexi ile çağrı yapılır [].join.call
 
-Peki neden çalışır?
-
-Çünkü gerçek dizinin join metodu oldukça basittir.
+Gerçek dizinin join metodu oldukça basittir.
 
 Tanımından “olduğu” gibi alındığında işlem şu şekildedir:
 
@@ -55,5 +57,3 @@ Tanımından “olduğu” gibi alındığında işlem şu şekildedir:
 8. Return result.
 
 Teknik olarak this'i alır ve this[0], this[1] …vs. şeklinde birleştirir. Bu şekilde yazılarak tüm dizi-gibi'lerin this şeklinde çalışmasını sağlar. Bundan dolayı this=arguments şeklinde de çalışır. */
-
-//TODO ÇALIŞ
