a5af2d8084c0c036a1d020f2f659dbeb65d4f03a
Nurcan Curebal <nurcancurebal@hotmail.com>
2022-09-19 21:10:13 +0300

    .

---
+++ prototypes-inheritance-prototipler-ve-kalitim/fprototype.js
@@ -0,0 +1,34 @@
+/* -> F.prototype
+Modern JavaScript’te protitip’i __proto__ kullanarak ( bir önceki bölümde anlatıldığı gibi ) ayarlayabiliriz. Fakat bu daha öncesinde böyle değildi.
+
+JavaScript başlangıcından beri kalıtıma sahipti. Bu dilin çekirdek özelliklerinden biriydi.
+
+Fakat bunu ayarlamak için sadece bir yol vardı ve bu yol yapıcıda(constructor)'da "prototype" kullanmaktı. Hala birçok kodda bu şekilde kullanılmaktadır. */
+
+
+/* -> “Prototip” özelliği
+Bildiğiniz gibi, new F() yeni bir obje oluşturur.
+
+new F() ile yeni bir obje yaratıldığında, obje’nin [[Prototype]]'ı F.prototype a ayarlanır.
+
+Diğer bir deyişle, eğer F prototype özelliğine sahip ve bu da obje tipine ayarlanmışsa, new operatörü bunu [[Prototype]] ayarlamak için kullanır.
+
+Aklınızda bulunsun F.prototype burada F'in sahip olduğu sıradan bir "prototype" objesidir. “prototype” terimine çok benzese de aslında burada gerçekten kullanılan sıradan bir objedir. */
+
+let animal = {
+
+    eats: true
+};
+
+function Rabbit(name) {
+
+    this.name = name;
+}
+
+Rabbit.prototype = animal;
+
+let rabbit = new Rabbit("White Rabbit"); //  rabbit.__proto__ == animal
+
+console.log(rabbit.eats); // true
+
+/* Rabbit.prototype = animal sözcük anlamıyla: “Eğer yeni bir Rabbit yaratılırsa new Rabbit, bunun [[Prototype]]ını animal'a ata” */
