7bd102fd5ac98f349f0771e371945c65ebc4e535
Nurcan Curebal <nurcancurebal@hotmail.com>
2022-09-21 15:48:00 +0300

    .

---
+++ classes-siniflar/sinif-ifadesi.js
@@ -0,0 +1,45 @@
+/* Tıpkı fonksiyonlar gibi, sınıflar da başka bir ifadenin içinde tanımlanabilir, dolaşılabilir, döndürülebilir, atanabilir vb. */
+
+let User1 = class {
+
+    sayHi() {
+
+        console.log("Hello");
+    }
+};
+
+/* Adlandırılmış İşlev İfadelerine benzer şekilde, sınıf ifadelerinin bir adı olabilir veya olmayabilir.
+
+Sınıf ifadesinin bir adı varsa, yalnızca sınıfın içinde görünür: */
+
+// "Named Class Expression" (spesifikasyonda böyle bir terim yok, ancak bu Adlandırılmış İşlev İfadesine benzer)
+let User = class MyClass {
+
+    sayHi() {
+
+        console.log(MyClass); // MyClass yalnızca sınıfın içinde görünür
+    }
+};
+
+new User().sayHi(); // çalışır, MyClass tanımını gösterir
+
+console.log(MyClass); // error, MyClass sınıfın dışında görünmez
+
+// Hatta sınıfları dinamik olarak "isteğe bağlı" hale getirebiliriz, şöyle:
+
+function makeClass(phrase) {
+
+    // bir sınıf ilan et ve geri ver
+    return class {
+
+        sayHi() {
+
+            console.log(phrase);
+        };
+    };
+}
+
+// Create a new class
+let User2 = makeClass("Hello");
+
+new User2().sayHi(); // Hello // TODO
